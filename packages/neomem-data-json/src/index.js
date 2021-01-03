1e2376555fde9527f907d104a37e7267edcc71d4
Brian Burns <bburns.km@gmail.com>
2021-01-03 08:09:30 -0600

    fix

--- packages/neomem-data-json/src/index.js
+++ packages/neomem-data-json/src/index.js
@@ -4,7 +4,7 @@ const resolvers = require('./resolvers')
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers, mocks })
+const server = new ApolloServer({ typeDefs, resolvers })
 
 // The `listen` method launches a web server.
 server.listen({ port: 4001 }).then(({ url }) => {
