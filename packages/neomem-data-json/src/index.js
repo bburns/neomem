eed91364db2e69e54e1d1348370618338cd6580e
Brian Burns <bburns.km@gmail.com>
2021-01-03 07:50:51 -0600

    orgfiles

--- packages/neomem-data-json/src/index.js
+++ packages/neomem-data-json/src/index.js
@@ -1,5 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('apollo-server')
 const Conf = require('conf')
+const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 
 const mocks = process.env.NODE_ENV === 'test'
@@ -8,25 +9,6 @@ const config = new Conf()
 config.set('unicorn', 'foooo')
 console.log(config.get('unicorn'))
 console.log(config.path)
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  type Node {
-    id: String
-    name: String
-    type: String
-    description: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    nodes: [Node]
-  }
-`
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
---
+++ packages/neomem-data-json/src/schema.js
@@ -0,0 +1,22 @@
+const { gql } = require('apollo-server')
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  type Node {
+    id: String
+    name: String
+    type: String
+    description: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    nodes: [Node]
+  }
+`
+
+module.exports = typeDefs
