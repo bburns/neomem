89131193647d6bce8e4c44251ff738739c5189a7
Brian Burns <bburns.km@gmail.com>
2021-01-05 13:58:35 -0600

    wip resolvers

--- packages/neomem-data-json/src/resolvers.js
+++ packages/neomem-data-json/src/resolvers.js
@@ -2,6 +2,7 @@ const Conf = require('conf')
 const nodes = require('./fixtures/nodes')
 
 const mocks = process.env.NODE_ENV === 'test'
+console.log('mocks', mocks)
 
 // conf reads/writes to a file in user's config folder, wherever that may be.
 // file:///Users/bburns/Library/Preferences/neomem-data-json-nodejs/config.json
@@ -10,15 +11,12 @@ console.log('config.path:', config.path)
 // config.set('1234', {})
 // console.log(config.get('1234'))
 
-// const nodesDict = {}
-// nodes.forEach(node => (nodesDict[node.id] = node))
-
 const resolvers = {
   Query: {
     info: () => `This is the GraphQL endpoint for a simple JSON file`,
     // authors: (obj, args, context, info) => authors,
     // test: (_, { id }) => id,
-    node: (_, { key }) => nodes[key],
+    node: (_, { key }) => (mocks ? nodes[key] : config.get(key)),
     nodes: () => Object.values(nodes),
     // books: (obj, args, context, info) => {
     //   console.log(context)
