4a223ef865acdecedc35ff8ea1bb5f2d20a6eb4f
Brian Burns <bburns.km@gmail.com>
2021-01-05 13:55:58 -0600

    move nodes to file

--- packages/neomem-data-json/src/resolvers.js
+++ packages/neomem-data-json/src/resolvers.js
@@ -1,40 +1,31 @@
 const Conf = require('conf')
+const nodes = require('./fixtures/nodes')
 
 const mocks = process.env.NODE_ENV === 'test'
 
-// conf reads/writes to a file in user's config folder,
-// wherever that may be.
+// conf reads/writes to a file in user's config folder, wherever that may be.
+// file:///Users/bburns/Library/Preferences/neomem-data-json-nodejs/config.json
 const config = new Conf()
-config.set('unicorn', 'foooo')
-console.log(config.get('unicorn'))
-console.log(config.path)
+console.log('config.path:', config.path)
+// config.set('1234', {})
+// console.log(config.get('1234'))
 
-// mock data
-const nodes = [
-  {
-    id: '838',
-    name: 'plecy',
-    type: 'fish',
-    description: 'plecostomus catfish',
-  },
-]
-
-const nodesDict = {}
-nodes.forEach(node => (nodesDict[node.id] = node))
+// const nodesDict = {}
+// nodes.forEach(node => (nodesDict[node.id] = node))
 
 const resolvers = {
   Query: {
     info: () => `This is the GraphQL endpoint for a simple JSON file`,
-    test: (_, { id }) => id,
-    node: (_, { key }) => nodesDict[key],
-    nodes: () => nodes,
+    // authors: (obj, args, context, info) => authors,
+    // test: (_, { id }) => id,
+    node: (_, { key }) => nodes[key],
+    nodes: () => Object.values(nodes),
     // books: (obj, args, context, info) => {
     //   console.log(context)
     //   return new Promise((resolve, reject) => {
     //     setTimeout(() => resolve(books), 500)
     //   })
     // },
-    // authors: (obj, args, context, info) => authors,
   },
 }
 
