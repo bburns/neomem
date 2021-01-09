d28944742f2d75673521735d7313d9e7c786ffa7
Brian Burns <bburns.km@gmail.com>
2021-01-09 01:37:57 -0600

    neomem-data fed

--- packages/neomem-data/src/index.js
+++ packages/neomem-data/src/index.js
@@ -1,16 +1,22 @@
 const { ApolloServer } = require('apollo-server')
-const typeDefs = require('./schema')
-const resolvers = require('./resolvers')
+const { ApolloGateway } = require('@apollo/gateway')
+require('dotenv').config()
 
-const server = new ApolloServer({
-  typeDefs,
-  // dataSources: () => ({
-  //   launchAPI: new LaunchAPI(),
-  // }),
-  resolvers,
+const mocks = process.env.NODE_ENV === 'test'
+
+// serviceList not needed with apollo studio
+// const gateway = new ApolloGateway()
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'neo4j', url: 'http://localhost:4001' },
+    { name: 'filesys', url: 'http://localhost:4002' },
+  ],
 })
 
-// launch web server
-server.listen({ port: 4001 }).then(({ url }) => {
-  console.log(`GraphQL json server ready at ${url}`)
+//. subscriptions not yet compatible with gateway, so must turn off
+const server = new ApolloServer({ gateway, subscriptions: false })
+
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`GraphQL neomem server ready at ${url}`)
 })
