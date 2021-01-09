328b5bb7dcae44926e8b4b9acef95b2be0dee298
Brian Burns <bburns.km@gmail.com>
2021-01-09 00:51:46 -0600

    work on neomem-data-json

--- packages/neomem-data-json/src/resolvers.js
+++ packages/neomem-data-json/src/resolvers.js
@@ -9,6 +9,9 @@ const mocks = process.env.NODE_ENV === 'test'
 // file:///Users/bburns/Library/Preferences/neomem-data-json-nodejs/config.json
 const config = new Conf()
 
+// add id to nodes
+Object.keys(nodes).forEach(id => (nodes[id].id = id))
+
 console.log('mocks', mocks)
 console.log('config.path:', config.path)
 
@@ -16,9 +19,9 @@ const resolvers = {
   Query: {
     info: () =>
       `This is the GraphQL endpoint for a simple JSON file (or js object if mocked)`,
-    // authors: (obj, args, context, info) => authors,
     get: (_, { id }) => (mocks ? nodes[id] : config.get(id)),
-    nodes: () => Object.values(nodes),
+    nodes: () => (mocks ? Object.values(nodes) : config.get()),
+    // authors: (obj, args, context, info) => authors,
     // books: (obj, args, context, info) => {
     //   return new Promise((resolve, reject) => {
     //     setTimeout(() => resolve(books), 500)
