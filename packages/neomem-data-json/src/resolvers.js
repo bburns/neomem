db8df8baf16d8788b6817b61f8e3f727702ba587
Brian Burns <bburns.km@gmail.com>
2021-01-09 10:46:06 -0600

    cleanup

--- packages/neomem-data-json/src/resolvers.js
+++ packages/neomem-data-json/src/resolvers.js
@@ -1,31 +1,19 @@
+// note: resolver functions take (parent, args, context, info) as params
+
 const resolvers = {
   Query: {
-    info: () =>
-      `This is a GraphQL endpoint for a JSON file (or js object if mocked)`,
-    // get: (_, { id }, { datasources }) => (mocks ? nodes[id] : config.get(id)),
+    info: () => `This is a GraphQL endpoint for a JSON file`,
     get: (_, { id }, { dataSources }) => dataSources.configAPI.get(id),
-    // nodes: () => (mocks ? Object.values(nodes) : Object.values(config.store)),
     nodes: (_, __, { dataSources }) =>
       Object.values(dataSources.configAPI.store),
-    // authors: (parent, args, context, info) => authors,
-    // books: (parent, args, context, info) => {
-    //   return new Promise((resolve, reject) => {
-    //     setTimeout(() => resolve(books), 500)
-    //   })
-    // },
   },
+
   Mutation: {
-    set: (parent, args, { dataSources }, info) => {
-      // console.log(parent, args, context, info)
+    set: (_, args, { dataSources }) => {
       const node = {
         name: args.name,
         description: args.description,
       }
-      // if (mocks) {
-      //   nodes[node.id] = node
-      // } else {
-      //   config.set(node.id, node)
-      // }
       dataSources.configAPI.set(node.id, node)
       return {
         success: true,
