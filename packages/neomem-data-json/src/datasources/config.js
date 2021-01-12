ab8fae2d4d305996d3ac225eb82f065ed174cf7d
Brian Burns <bburns.km@gmail.com>
2021-01-11 19:20:44 -0600

    cmt

--- packages/neomem-data-json/src/datasources/config.js
+++ packages/neomem-data-json/src/datasources/config.js
@@ -1,3 +1,8 @@
+// apollo config file datasource.
+// apollo lets you define datasource and pass them to your resolvers.
+// it's not strictly necessary, as you can define the datasource in the resolver files.
+// but maybe it helps keep resolver file more concise?
+
 const { DataSource } = require('apollo-datasource')
 
 class ConfigAPI extends DataSource {
