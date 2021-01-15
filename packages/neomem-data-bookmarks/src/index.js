6abdb122525476ed9a9ce48f13b9930ef9c05ce8
Brian Burns <bburns.km@gmail.com>
2021-01-15 10:39:37 -0600

    mv

--- packages/neomem-data-bookmarks/src/index.js
+++ packages/neomem-data-bookmarks/src/index.js
@@ -4,18 +4,15 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 const BookmarksAPI = require('./datasources/bookmarks')
-// const fs = require('fs')
-// const JSONStream = require('JSONStream')
+const fs = require('fs')
 
 // read and parse json file directly using require.
 // but only works for json files with .json extension -
 // otherwise thinks it's javascript.
 // see https://stackoverflow.com/a/36591002/243392
-// const path = './foo.json'
-// const bookmarks = require(path)
+// const bookmarks = require('./example.json')
 const path =
   '/Users/bburns/Library/Application Support/Google/Chrome/Default/Bookmarks.bak'
-const fs = require('fs')
 const bookmarks = JSON.parse(fs.readFileSync(path, 'utf-8'))
 
 const server = new ApolloServer({
