fa88af70cf9ce0e3e010d60e3deaa17817782b6c
Brian Burns <bburns.km@gmail.com>
2021-01-17 10:47:05 -0600

    mv cmd to js

---
+++ packages/neomem-console/src/commands.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch')
+const Table = require('easy-table')
+
+const uri = 'http://localhost:4101'
+
+async function list() {
+  const query = `query { node { name, type, guid, url, date_added, date_modified }}`
+  const json = await fetchQuery(query)
+  const nodes = json.data.node
+  const t = new Table()
+  nodes.forEach(node => {
+    t.cell('Name', node.name)
+    t.cell('Type', node.type)
+    t.cell('Url', node.url)
+    t.cell('Date Added', node.date_added)
+    t.cell('Date Modified', node.date_modified)
+    // t.cell('Guid', node.guid)
+    t.newRow()
+  })
+  const s = t.toString()
+  // const s = t.printTransposed()
+  console.log(s)
+}
+
+async function fetchQuery(query) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept-Encoding': 'gzip',
+    },
+    body: JSON.stringify({ query }),
+  }
+  const response = await fetch(uri, options)
+  const json = await response.json()
+  return json
+}
+
+module.exports = { list }
--- packages/neomem-console/src/index.js
+++ packages/neomem-console/src/index.js
@@ -5,49 +5,19 @@
 // currently just lets you enter a graphql query directly.
 
 const repl = require('repl')
-const fetch = require('node-fetch')
-const Table = require('easy-table')
+const commands = require('./commands')
 
 // const prompt = '|> '
 const prompt = '[neomem] > '
-const uri = 'http://localhost:4101'
 
 repl.start({ prompt, eval: evalCommand })
 
 async function evalCommand(cmd, context, filename, callback) {
-  const query = `query { node { name, type, guid, url, date_added, date_modified }}`
-  const json = await fetchQuery(query)
-  const nodes = json.data.node
-  const t = new Table()
-  nodes.forEach(node => {
-    t.cell('Name', node.name)
-    t.cell('Type', node.type)
-    t.cell('Url', node.url)
-    t.cell('Date Added', node.date_added)
-    t.cell('Date Modified', node.date_modified)
-    // t.cell('Guid', node.guid)
-    t.newRow()
-  })
-  const s = t.toString()
-  // const s = t.printTransposed()
-  console.log(s)
-  // call callback so knows to print prompt
-  // wraps output string in quotes, so don't use it here
+  const command = commands[cmd.trim()]
+  await command()
+  // need to call callback so it knows to print prompt again.
+  // wraps output in quotes, so don't use that here.
   const error = null
   const output = null
   callback(error, output)
-}
-
-async function fetchQuery(query) {
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept-Encoding': 'gzip',
-    },
-    body: JSON.stringify({ query }),
-  }
-  const response = await fetch(uri, options)
-  const json = await response.json()
-  return json
 }
