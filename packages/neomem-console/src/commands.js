2bfb9f6aa46e1d506e73e105be38f05ccd24f342
Brian Burns <bburns.km@gmail.com>
2021-01-17 17:19:26 -0600

    cleanup

--- packages/neomem-console/src/commands.js
+++ packages/neomem-console/src/commands.js
@@ -1,4 +1,3 @@
-// const Table = require('easy-table')
 const fetchQuery = require('./fetchQuery')
 const Table = require('./table')
 
@@ -6,17 +5,7 @@ async function list() {
   const query = `query { node { name, type, url, date_added, depth }}`
   const json = await fetchQuery(query)
   const nodes = json.data.node
-  // const t = new Table()
-  // nodes.forEach(node => {
-  //   t.cell('Name', node.name)
-  //   t.cell('Type', node.type)
-  //   t.cell('Url', node.url)
-  //   t.cell('Date Added', node.date_added)
-  //   t.newRow()
-  // })
-  // const s = t.toString()
   const columns = [
-    // { name: 'Name', accessor: 'name', width: 36 },
     {
       name: 'Name',
       accessor: obj => ' '.repeat(obj.depth) + obj.name,
@@ -30,23 +19,5 @@ async function list() {
   const s = t.toString()
   console.log(s)
 }
-
-// async function compare() {
-//   const query = `query { node { name, type, guid, url, date_added, date_modified }}`
-//   const json = await fetchQuery(query)
-//   const nodes = json.data.node
-//   const t = new Table()
-//   nodes.forEach(node => {
-//     t.cell('Name', node.name)
-//     t.cell('Type', node.type)
-//     t.cell('Url', node.url)
-//     t.cell('Date Added', node.date_added)
-//     t.cell('Date Modified', node.date_modified)
-//     t.cell('Guid', node.guid)
-//     t.newRow()
-//   })
-//   const s = t.printTransposed()
-//   console.log(s)
-// }
 
 module.exports = { list }
