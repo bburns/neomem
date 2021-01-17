48fcbec03999e990f9c3e429b57b4a74ebfb5e1a
Brian Burns <bburns.km@gmail.com>
2021-01-17 17:13:29 -0600

    mv code to js

--- packages/neomem-console/src/commands.js
+++ packages/neomem-console/src/commands.js
@@ -1,50 +1,28 @@
 // const Table = require('easy-table')
-const { table, getBorderCharacters } = require('table')
 const fetchQuery = require('./fetchQuery')
+const Table = require('./table')
 
 async function list() {
-  const query = `query { node { name, type, guid, url, date_added, date_modified, depth }}`
+  const query = `query { node { name, type, url, date_added, depth }}`
   const json = await fetchQuery(query)
   const nodes = json.data.node
   // const t = new Table()
   // nodes.forEach(node => {
   //   t.cell('Name', node.name)
-  //   t.cell('depth', node.depth)
   //   t.cell('Type', node.type)
   //   t.cell('Url', node.url)
   //   t.cell('Date Added', node.date_added)
-  //   t.cell('Date Modified', node.date_modified)
   //   t.newRow()
   // })
   // const s = t.toString()
-  const arr = Object.values(nodes).map(node => [
-    '--'.repeat(node.depth) + node.name,
-    node.type,
-    node.url,
-    node.date_added,
-    // node.date_modified,
-    // node.depth,
-  ])
-  const headings = `Name,Type,Url,Date Added`.split(',')
-  const arr2 = [headings, ...arr]
-  const wrapWord = true
-  const options = {
-    columnDefault: {
-      width: 20,
-      truncate: 20,
-    },
-    columns: {
-      0: { width: 30, truncate: 30, wrapWord },
-      1: { width: 8, truncate: 8, wrapWord },
-      2: { width: 30, truncate: 30, wrapWord },
-      // 5: { width: 8, truncate: 8, wrapWord },
-    },
-    // singleLine: true,
-    border: getBorderCharacters(`ramac`),
-    drawHorizontalLine: (index, size) =>
-      index === 0 || index === 1 || index === size,
-  }
-  const s = table(arr2, options)
+  const columns = [
+    { name: 'Name', accessor: 'name', width: 36 },
+    { name: 'Type', accessor: 'type', width: 8 },
+    { name: 'Url', accessor: 'url', width: 20 },
+    { name: 'Date Added', accessor: 'date_added', width: 30 },
+  ]
+  const t = new Table(columns, nodes)
+  const s = t.toString()
   console.log(s)
 }
 
