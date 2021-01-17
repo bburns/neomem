7949d13bd6eaaafc2efe836fafeac8056a821d92
Brian Burns <bburns.km@gmail.com>
2021-01-17 09:23:01 -0600

    wip console list

--- packages/neomem-console/src/index.js
+++ packages/neomem-console/src/index.js
@@ -1,12 +1,15 @@
 // neomem-console
+
 // this repl (read-eval-print-loop) will translate from english-like language
 // to graphql queries, and format the results nicely.
 // currently just lets you enter a graphql query directly.
 
 const repl = require('repl')
 const fetch = require('node-fetch')
+const Table = require('easy-table')
 
-const prompt = '|> '
+// const prompt = '|> '
+const prompt = '[neomem] > '
 const uri = 'http://localhost:4101'
 
 repl.start({ prompt, eval: evalCommand })
@@ -14,8 +17,21 @@ repl.start({ prompt, eval: evalCommand })
 async function evalCommand(cmd, context, filename, callback) {
   const query = `query { node { name, type, guid, url, date_added, date_modified }}`
   const json = await fetchQuery(query)
-  const s = JSON.stringify(json)
-  callback(null, s)
+  const nodes = json.data.node
+  // const s = JSON.stringify(nodes)
+  const t = new Table()
+  nodes.forEach(node => {
+    t.cell('Name', node.name)
+    t.cell('Type', node.type)
+    t.cell('Url', node.url)
+    t.cell('Date Added', node.date_added)
+    t.cell('Date Modified', node.date_modified)
+    // t.cell('Guid', node.guid)
+    t.newRow()
+  })
+  const s = t.toString()
+  const error = null
+  callback(error, s)
 }
 
 async function fetchQuery(query) {
