b56a96b00f809db765db05acec0b1395f7749da1
Brian Burns <bburns.km@gmail.com>
2021-01-17 10:48:22 -0600

    add compare cmd

--- packages/neomem-console/src/commands.js
+++ packages/neomem-console/src/commands.js
@@ -22,6 +22,25 @@ async function list() {
   console.log(s)
 }
 
+async function compare() {
+  const query = `query { node { name, type, guid, url, date_added, date_modified }}`
+  const json = await fetchQuery(query)
+  const nodes = json.data.node
+  const t = new Table()
+  nodes.forEach(node => {
+    t.cell('Name', node.name)
+    t.cell('Type', node.type)
+    t.cell('Url', node.url)
+    t.cell('Date Added', node.date_added)
+    t.cell('Date Modified', node.date_modified)
+    t.cell('Guid', node.guid)
+    t.newRow()
+  })
+  // const s = t.toString()
+  const s = t.printTransposed()
+  console.log(s)
+}
+
 async function fetchQuery(query) {
   const options = {
     method: 'POST',
@@ -36,4 +55,4 @@ async function fetchQuery(query) {
   return json
 }
 
-module.exports = { list }
+module.exports = { list, compare }
