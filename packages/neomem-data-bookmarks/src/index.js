71be576f0697c03963edeaffb4639034c8b8f208
Brian Burns <bburns.km@gmail.com>
2021-01-17 18:06:26 -0600

    mv files

--- packages/neomem-data-bookmarks/src/index.js
+++ packages/neomem-data-bookmarks/src/index.js
@@ -11,8 +11,7 @@ const fs = require('fs')
 // but only works for files with .json extension -
 // otherwise it thinks it's javascript.
 // see https://stackoverflow.com/a/36591002/243392
-// const chromePath =
-// '/Users/bburns/Library/Application Support/Google/Chrome/Default/Bookmarks.bak'
+// const chromePath = '/Users/bburns/Library/Application Support/Google/Chrome/Default/Bookmarks'
 const chromePath = __dirname + '/../test/fixtures/example2.json'
 const examplePath = __dirname + '/../test/fixtures/example.json'
 const path = options.use === 'chrome' ? chromePath : examplePath
