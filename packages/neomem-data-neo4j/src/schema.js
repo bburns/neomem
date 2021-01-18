a6b9784870105b01f6bdafb546390525e54f41a7
Brian Burns <bburns.km@gmail.com>
2021-01-18 12:12:38 -0600

    add Book type

--- packages/neomem-data-neo4j/src/schema.js
+++ packages/neomem-data-neo4j/src/schema.js
@@ -2,21 +2,31 @@
 
 const { gql } = require('apollo-server')
 
-// const types = `Node,Book,Fish`.split(',')
-
-const typeDefs = gql`
-  type Node {
-    uuid: ID!
-    depth: Int
-    name: String
-    notes: String
-    created: String
-    modified: String
-  }
-
-  type Fish {
-    name: String
-  }
+const types = `Node,Book,Fish`.split(',')
+const fields = `
+  uuid: ID!
+  depth: Int
+  name: String
+  notes: String
+  created: String
+  modified: String
 `
+const s = types.map(type => `type ${type} {${fields}}`).join('\n')
+console.log(s)
+const typeDefs = gql(s)
+// const typeDefs = gql`
+//   type Node {
+//     uuid: ID!
+//     depth: Int
+//     name: String
+//     notes: String
+//     created: String
+//     modified: String
+//   }
+
+//   type Fish {
+//     name: String
+//   }
+// `
 
 module.exports = typeDefs
