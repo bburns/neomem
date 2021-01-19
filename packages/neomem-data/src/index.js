7d9d7140d4dc19d1d65bd7d73dfcc4af3ee47454
Brian Burns <bburns.km@gmail.com>
2021-01-19 04:28:30 -0600

    wip

--- packages/neomem-data/src/index.js
+++ packages/neomem-data/src/index.js
@@ -4,36 +4,30 @@ const { gql, ApolloServer } = require('apollo-server')
 const fetch = require('node-fetch')
 
 const typeDefs = gql`
-  type Foo {
+  type Node {
     name: String
   }
   type Query {
-    foo: [Foo]
+    neo4j: [Node]
   }
 `
 const body = { query: 'query { Fish { name }}' }
 
 const resolvers = {
   Query: {
-    // foo: _ => [{ name: 'pokpok' }],
-    foo: async _ => {
+    neo4j: async (parent, args, context, info) => {
+      // console.log(parent, args, context, info) // undefined, {}, bleh, {...}
+      console.log(info.path)
       const response = await fetch('http://localhost:4102', {
         method: 'POST',
         body: JSON.stringify(body),
         headers: { 'Content-Type': 'application/json' },
       })
-      // console.log(response)
-      // return response
       const json = await response.json()
-      console.log(json)
-      // return json
       const fishes = json.data.Fish
       return fishes
     },
   },
-  // Foo: {
-  //   name: _ => 'bradley',
-  // },
 }
 
 // make apollo server
