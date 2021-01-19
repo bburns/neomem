07f30dc1ea90496297a2540da739d7e8dbea6188
Brian Burns <bburns.km@gmail.com>
2021-01-19 04:45:28 -0600

    pass subquery as argument

--- packages/neomem-data/src/index.js
+++ packages/neomem-data/src/index.js
@@ -4,26 +4,32 @@ const { gql, ApolloServer } = require('apollo-server')
 const fetch = require('node-fetch')
 
 const typeDefs = gql`
-  type Node {
+  type Foo {
     name: String
   }
   type Query {
-    neo4j: [Node]
+    neo4j(subquery: String): [Foo]
   }
 `
-const body = { query: 'query { Fish { name }}' }
+// const body = { query: 'query { Fish { name }}' }
 
 const resolvers = {
   Query: {
     neo4j: async (parent, args, context, info) => {
       // console.log(parent, args, context, info) // undefined, {}, bleh, {...}
-      console.log(info.path)
+      console.log(args.subquery) // "query{Fish{name}}"
+      console.log(info.fieldName) // "neo4j"
+      console.log(info.path) // { key: 'neo4j', ...}
+      const body = { query: args.subquery }
       const response = await fetch('http://localhost:4102', {
         method: 'POST',
         body: JSON.stringify(body),
         headers: { 'Content-Type': 'application/json' },
       })
+      // console.log(response)
       const json = await response.json()
+      // console.log(json)
+      console.log(json.data)
       const fishes = json.data.Fish
       return fishes
     },
