38cce7ec1abd94b7310c0e60007c36dd5e16b20f
Brian Burns <bburns.km@gmail.com>
2021-01-19 10:51:48 -0600

    cmts

--- packages/neomem-data/src/index.js
+++ packages/neomem-data/src/index.js
@@ -1,4 +1,5 @@
 // neomem-data
+// a simple federated graphql server
 
 const { gql, ApolloServer } = require('apollo-server')
 const fetch = require('node-fetch')
@@ -7,17 +8,23 @@ const GraphQLJSON = require('graphql-type-json')
 // define datasource mount points
 // the key is how the datasource is referred to from nmconsole,
 // plugin is which datasource to query.
+//. this will eventually be stored in filesystem somewhere.
+//. on running this app, will scan these plugins to see if still there,
+// and poll every n secs to see if still there.
+// on running a plugin it will call a mutate method here to add it to the list.
 const mounts = {
   neo4j: { plugin: 'neo4j', url: 'http://localhost:4101' },
   bookmarks: { plugin: 'bookmarks', url: 'http://localhost:4103' },
 }
 
-// define queries, eg "bookmarks(subquery: String): JSON"
+// define queries, eg "bookmarks(subquery: String): JSON".
+// these just pass along a graphql query to the given mount.
+// each query just resolves to a JSON object
 const queries = Object.keys(mounts)
   .map(mountKey => `${mountKey}(subquery: String): JSON`)
   .join('\n')
 
-// define types
+// define schema / type defs
 const typeDefs = gql`
   scalar JSON
   type Query {
@@ -25,17 +32,19 @@ const typeDefs = gql`
   }
 `
 
-// define query resolver for each mount point
+// define the query resolver for each mount point.
+// each takes (parent, args, context, info), fetches data from the
+// mounted graphql server, and returns the result as json.
 const Query = {}
 for (const [mountKey, mount] of Object.entries(mounts)) {
   const resolver = async (parent, args, context, info) => {
-    const body = { query: args.subquery }
+    const body = { query: args.subquery } // the graphql query
     const response = await fetch(mount.url, {
       method: 'POST',
       body: JSON.stringify(body),
       headers: { 'Content-Type': 'application/json' },
     })
-    const json = await response.json()
+    const json = await response.json() // convert result to json object
     return json
   }
   Query[mountKey] = resolver
@@ -51,6 +60,7 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers })
 
 // start the server
+//. pass port as option
 server.listen({ port: 4100 }).then(({ url }) => {
   console.log(`GraphQL neomem server ready at ${url}`)
 })
