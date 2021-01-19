41e5863f5caa23c74682af91233d9e852612c1f3
Brian Burns <bburns.km@gmail.com>
2021-01-19 04:20:18 -0600

    handoff foo query to 4102 neo4j endpoint

--- packages/neomem-data/src/index.js
+++ packages/neomem-data/src/index.js
@@ -1,6 +1,7 @@
 // neomem-data
 
 const { gql, ApolloServer } = require('apollo-server')
+const fetch = require('node-fetch')
 
 const typeDefs = gql`
   type Foo {
@@ -10,14 +11,29 @@ const typeDefs = gql`
     foo: [Foo]
   }
 `
+const body = { query: 'query { Fish { name }}' }
 
 const resolvers = {
   Query: {
-    foo: _ => [{ name: 'pokpok' }],
+    // foo: _ => [{ name: 'pokpok' }],
+    foo: async _ => {
+      const response = await fetch('http://localhost:4102', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      // console.log(response)
+      // return response
+      const json = await response.json()
+      console.log(json)
+      // return json
+      const fishes = json.data.Fish
+      return fishes
+    },
   },
-  Foo: {
-    name: _ => 'bradley',
-  },
+  // Foo: {
+  //   name: _ => 'bradley',
+  // },
 }
 
 // make apollo server
