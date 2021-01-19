63052d1c5437b64b2e1f96be3751d622441ca425
Brian Burns <bburns.km@gmail.com>
2021-01-19 05:15:11 -0600

    return json object in bookmarks subquery

--- packages/neomem-data/src/index.js
+++ packages/neomem-data/src/index.js
@@ -2,19 +2,30 @@
 
 const { gql, ApolloServer } = require('apollo-server')
 const fetch = require('node-fetch')
+const { GraphQLJSON, GraphQLJSONObject } = require('graphql-type-json')
+
+const types = {
+  neo4j: { port: 4101 },
+  // filesys: { port: 4102 },
+  bookmarks: { port: 4103 },
+}
 
 const typeDefs = gql`
+  scalar JSON
   type Foo {
     name: String
   }
   type Query {
     neo4j(subquery: String): [Foo]
-    filesys(subquery: String): [Foo]
-    bookmarks(subquery: String): [Foo]
+    # filesys(subquery: String): [Foo]
+    # bookmarks(subquery: String): [Foo]
+    bookmarks(subquery: String): JSON
+    # bookmarks(subquery: String): String
   }
 `
 
 const resolvers = {
+  JSON: GraphQLJSON,
   Query: {
     neo4j: async (parent, args, context, info) => {
       // console.log(parent, args, context, info) // undefined, {}, bleh, {...}
@@ -31,17 +42,17 @@ const resolvers = {
       const nodes = json.data.Node
       return nodes
     },
-    filesys: async (parent, args, context, info) => {
-      const body = { query: args.subquery }
-      const response = await fetch('http://localhost:4102', {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' },
-      })
-      const json = await response.json()
-      const nodes = json.data.node
-      return nodes
-    },
+    // filesys: async (parent, args, context, info) => {
+    //   const body = { query: args.subquery }
+    //   const response = await fetch('http://localhost:4102', {
+    //     method: 'POST',
+    //     body: JSON.stringify(body),
+    //     headers: { 'Content-Type': 'application/json' },
+    //   })
+    //   const json = await response.json()
+    //   const nodes = json.data.node
+    //   return nodes
+    // },
     bookmarks: async (parent, args, context, info) => {
       const body = { query: args.subquery }
       const response = await fetch('http://localhost:4103', {
@@ -50,8 +61,11 @@ const resolvers = {
         headers: { 'Content-Type': 'application/json' },
       })
       const json = await response.json()
-      const nodes = json.data.node
-      return nodes
+      console.log(json)
+      // return 'hi'
+      return json
+      // const nodes = json.data.node
+      // return nodes
     },
   },
 }
