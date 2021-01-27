12d6f71aace1439b1137b2a432b71f764d39e803
Brian Burns <bburns.km@gmail.com>
2021-01-27 03:03:20 -0600

    wip

--- packages/neomem-data-bookmarks/src/index.js
+++ packages/neomem-data-bookmarks/src/index.js
@@ -2,17 +2,42 @@
 
 const Hapi = require('@hapi/hapi')
 
+const fs = require('fs')
+// const options = require('./options')
+
+// read bookmarks
+// note: can read and parse json file directly using require,
+// but only works for files with .json extension -
+// otherwise it thinks it's javascript.
+// see https://stackoverflow.com/a/36591002/243392
+// const chromePath = '/Users/bburns/Library/Application Support/Google/Chrome/Default/Bookmarks'
+// const examplePath = __dirname + '/../test/fixtures/example.json'
+// const chromePath = __dirname + '/../test/fixtures/example2.json'
+// const path = options.use === 'chrome' ? chromePath : examplePath
+// console.log(`Reading ${path}...`)
+const path = __dirname + '/data/example.json'
+const bookmarks = JSON.parse(fs.readFileSync(path, 'utf-8'))
+
 const init = async () => {
   const server = Hapi.server({
-    port: 6000,
+    port: 4003,
     host: 'localhost',
   })
 
+  // server.route({
+  //   method: 'GET',
+  //   path: '/hello/{name}',
+  //   handler: (request, h) => {
+  //     return `Hello ${request.params.name}!`
+  //   },
+  // })
+
+  // this handles both localhost:4003 and localhost:4003/
   server.route({
     method: 'GET',
-    path: '/hello/{name}',
+    path: '/',
     handler: (request, h) => {
-      return `Hello ${request.params.name}!`
+      return `Hello world!\n`
     },
   })
 
@@ -21,7 +46,7 @@ const init = async () => {
     path: '/api/v1',
     handler: (request, h) => {
       const data = {
-        name: 'neomem-data',
+        name: 'neomem-data-bookmarks',
         type: 'datasource',
         description: 'a simple datasource saved to json file',
       }
@@ -33,13 +58,7 @@ const init = async () => {
     method: 'GET',
     path: '/api/v1/',
     handler: (request, h) => {
-      const data = [
-        {
-          name: 'neo4j',
-          type: 'datasource',
-          description: 'rest api for a neo4j database',
-        },
-      ]
+      const data = bookmarks
       return data
     },
   })
