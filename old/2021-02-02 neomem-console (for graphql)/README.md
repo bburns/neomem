1f322a641a556644d2a17f591c9e73a0a15ce67d
Brian Burns <bburns.km@gmail.com>
2021-02-02 02:12:22 -0600

    cp files

---
+++ old/2021-02-02 neomem-console (for graphql)/README.md
@@ -0,0 +1,5 @@
+# neomem-console
+
+
+use |> for prompt?
+
---
+++ old/2021-02-02 neomem-console (for graphql)/src/table.js
@@ -0,0 +1,43 @@
+const { table, getBorderCharacters } = require('table') // https://github.com/gajus/table
+
+class Table {
+  constructor(columns, objs) {
+    this.columns = columns
+    this.objs = objs
+  }
+
+  toString() {
+    const headings = this.columns.map(col => col.name)
+    const rows = [headings]
+
+    for (const obj of this.objs) {
+      const row = []
+      for (const col of this.columns) {
+        if (typeof col.accessor === 'function') {
+          row.push(col.accessor(obj))
+        } else {
+          row.push(obj[col.accessor])
+        }
+      }
+      rows.push(row)
+    }
+
+    const columnOptions = {}
+    for (let i = 0; i < this.columns.length; i++) {
+      const column = this.columns[i]
+      columnOptions[i] = { width: column.width, truncate: column.width }
+    }
+    const options = {
+      columns: columnOptions,
+      // singleLine: true,
+      border: getBorderCharacters('ramac'),
+      drawHorizontalLine: (index, size) =>
+        index === 0 || index === 1 || index === size,
+    }
+
+    const s = table(rows, options)
+    return s
+  }
+}
+
+module.exports = Table
