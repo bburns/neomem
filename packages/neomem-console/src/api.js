f5b3b9ecad07d6bb9b84679a5314922db4e281a1
Brian Burns <bburns.km@gmail.com>
2021-02-02 02:28:40 -0600

    list cmd works primitively

--- packages/neomem-console/src/api.js
+++ packages/neomem-console/src/api.js
@@ -2,20 +2,21 @@ const fetch = require('node-fetch') // mimics browser's fetch
 
 // define nmdata endpoint
 //. pass as parameter
-const uri = 'http://localhost:4000'
-console.log('uri', uri)
+const baseUrl = 'http://localhost:4000/api/v1/'
+console.log('baseUrl', baseUrl)
 
 // get json from the given uri
 async function get(query) {
+  const s = `${query.path}?fields=name,type,url`
   const options = {
     method: 'GET',
     headers: {
-      // 'Content-Type': 'application/json',
       'Accept-Encoding': 'gzip',
     },
-    // body: JSON.stringify(body),
   }
-  const response = await fetch(uri, options)
+  const url = baseUrl + s
+  console.log('fetch', url)
+  const response = await fetch(url, options)
   const json = await response.json()
   return json
 }
--- packages/neomem-console/src/commands.js
+++ packages/neomem-console/src/commands.js
@@ -6,31 +6,38 @@ const api = require('./api')
 const Table = require('./table') // wrapper around gajus table library
 
 async function list(tokens) {
-  const type = tokens[1] || 'Node' //.
+  // const type = tokens[1] || 'Node' //.
   // const query = `query { node { name, type, url, date_added, depth }}` // bookmarks
   // const query = `query { ${type} { name, notes, created, modified, depth }}` // neo4j
-  const query = `query { bookmarks(subquery:"query{node{name}}")}`
+  // const query = `query { bookmarks(subquery:"query{node{name}}")}`
+  // const query = `bookmarks?fields=name,type,url&sortby=name&limit=5`
+  const query = {
+    path: 'bookmarks',
+    fields: ['name', 'type', 'url'],
+    sortby: ['name'],
+    limit: 5,
+  }
   console.log(query)
-  const json = await api.fetch(query)
-  // console.log('json', json)
-  const data = json.data.bookmarks.data //.
+  const json = await api.get(query)
+  console.log('json', json)
+  // const data = json.data.bookmarks.data //.
   // console.log('data', data)
-  const nodes = data.node
-  const columns = [
-    {
-      name: 'Name',
-      accessor: obj => ' '.repeat(obj.depth) + obj.name,
-      width: 36,
-    },
-    { name: 'Type', accessor: 'type', width: 10 },
-    { name: 'Notes', accessor: 'notes', width: 20 },
-    { name: 'Created', accessor: 'created', width: 18 },
-    { name: 'Modified', accessor: 'modified', width: 18 },
-  ]
-  const t = new Table(columns, nodes)
-  const s = t.toString()
-  console.log(s)
-  console.log('done')
+  // const nodes = data.node
+  // const columns = [
+  //   {
+  //     name: 'Name',
+  //     accessor: obj => ' '.repeat(obj.depth) + obj.name,
+  //     width: 36,
+  //   },
+  //   { name: 'Type', accessor: 'type', width: 10 },
+  //   { name: 'Notes', accessor: 'notes', width: 20 },
+  //   { name: 'Created', accessor: 'created', width: 18 },
+  //   { name: 'Modified', accessor: 'modified', width: 18 },
+  // ]
+  // const t = new Table(columns, nodes)
+  // const s = t.toString()
+  // console.log(s)
+  // console.log('done')
 }
 
 module.exports = { list }
