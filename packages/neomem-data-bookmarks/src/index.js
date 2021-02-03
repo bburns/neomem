cc46a4a67675c7fec78b912420400c2d9b093b75
Brian Burns <bburns.km@gmail.com>
2021-02-03 10:16:58 -0600

    handle bookmark created/modified fields

--- packages/neomem-data-bookmarks/src/index.js
+++ packages/neomem-data-bookmarks/src/index.js
@@ -85,13 +85,32 @@ function getNodes(node, query) {
       .filter(node => node.name.includes(query.q) || node.url.includes(query.q))
       .map(node => {
         const projection = {}
-        //. convert chrome dates to iso dates here?
-        query.fields.forEach(field => (projection[field] = node[field]))
-        console.log(projection)
+        query.fields.forEach(field => {
+          //. convert chrome dates to iso dates here
+          if (field === 'created') {
+            projection[field] = getISODate(node.date_added)
+          } else if (field === 'modified') {
+            projection[field] = getISODate(node.date_modified)
+          } else {
+            projection[field] = node[field]
+          }
+        })
         return projection
       })
     return nodes
   }
   const node2 = node.children.find(child => child.name === first)
   return getNodes(node2, { ...query, path: rest })
+}
+
+// convert from 1601-based datestring to iso string.
+// chrome bookmark times are relative to 1601-01-01.
+// from https://stackoverflow.com/questions/51343828/how-to-parse-chrome-bookmarks-date-added-value-to-a-date
+const dateStart1601 = Date.UTC(1601, 0, 1)
+function getISODate(dateString1601) {
+  if (dateString1601) {
+    const date = new Date(dateStart1601 + Number(dateString1601) / 1000)
+    return date.toISOString()
+  }
+  return ''
 }
