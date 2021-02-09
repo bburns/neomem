01cfdd7ef282d9cf3d17737b3338c6d925a031c6
Brian Burns <bburns.km@gmail.com>
2021-02-09 01:34:54 -0600

    date1601

--- packages/neomem-util/test/types.test.js
+++ packages/neomem-util/test/types.test.js
@@ -1,12 +1,17 @@
 const test = require('ava')
 const { Types } = require('../src')
 
+const date1601 = Types.get('date1601') // get a type
+
 test('keys', t => {
-  // t.deepEqual(Object.keys(Types.dict).sort(), ['date1601', 'string'])
-  const date1601 = Types.get('date1601')
   t.is(date1601.key, 'date1601')
   t.deepEqual(
     Object.keys(date1601).sort(),
     'description format key parse type'.split(' ')
   )
+})
+
+test('conversions', t => {
+  t.is(date1601.format('2021-02-09'), 'NotYetImplemented')
+  t.is(date1601.parse('0'), '1601-01-01T00:00:00.000Z')
 })
