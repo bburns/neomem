62efcd1db2db0eba412c015abd872661dd88e90a
Brian Burns <bburns.km@gmail.com>
2021-02-09 01:49:53 -0600

    query testing

--- packages/neomem-util/test/query.test.js
+++ packages/neomem-util/test/query.test.js
@@ -1,16 +1,22 @@
 const test = require('ava')
-const { Query } = require('../src')
+const querystring = require('querystring')
+const { Query, Path } = require('../src')
 
-test('keys', t => {
+test('test all parts', t => {
+  const baseUrl = 'http://localhost:4000/api/v1'
+  const pathString = '/bookmarks/books/scifi'
+  const paramsString = 'fields=name,type,url&sortby=name'
+  const url = baseUrl + pathString + '?' + paramsString
   const request = {
-    params: { path: 'bookmarks' },
-    raw: {
-      req: {
-        url:
-          'localhost:4000/api/v1/bookmarks/books/scifi?fields=name,type,url&sortby=name',
-      },
-    },
+    params: { path: pathString },
+    raw: { req: { url } },
   }
+  const path = Path.make(pathString)
+  const params = querystring.parse(paramsString)
+  params.depth = 1 // add depth
   const query = Query.make(request)
-  t.deepEqual(query, {})
+  t.deepEqual(query.params, params)
+  t.deepEqual(query.paramsString, paramsString)
+  t.deepEqual(query.path, path)
+  t.deepEqual(query.url, url)
 })
