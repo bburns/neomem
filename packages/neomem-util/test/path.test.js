b6da02c8874949e0ec88c8e5b0f806d262258b35
Brian Burns <bburns.km@gmail.com>
2021-02-18 03:40:27 -0600

    cleanup

--- packages/neomem-util/test/path.test.js
+++ packages/neomem-util/test/path.test.js
@@ -1,12 +1,26 @@
 const test = require('ava').default
 const { Path } = require('../src')
 
-test(`Path.join - should join two plain paths`, async t => {
+test(`Path.make - handle abs path`, async t => {
+  const path = Path.make('/pok/lkm')
+  t.deepEqual(path.str, '/pok/lkm')
+  t.deepEqual(path.first, '')
+  t.deepEqual(path.rest, 'pok/lkm')
+})
+
+test(`Path.make - handle rel path`, async t => {
+  const path = Path.make('pok/lkm')
+  t.deepEqual(path.str, 'pok/lkm')
+  t.deepEqual(path.first, 'pok')
+  t.deepEqual(path.rest, 'lkm')
+})
+
+test(`Path.join - should join two rel paths`, async t => {
   const path = Path.join('pok', 'lkm')
   t.deepEqual(path.str, 'pok/lkm')
 })
 
-test(`Path.join - should join abs + plain paths`, async t => {
+test(`Path.join - should join abs + rel paths`, async t => {
   const path = Path.join('/pok', 'lkm')
   t.deepEqual(path.str, '/pok/lkm')
 })
@@ -16,10 +30,9 @@ test(`Path.join - should handle two abs paths`, async t => {
   t.deepEqual(path.str, '/lkm')
 })
 
-test(`Path.join - should handle plain + abs path`, async t => {
+test(`Path.join - should handle rel + abs path`, async t => {
   const path = Path.join('pok', '/lkm')
   t.deepEqual(path.str, '/lkm')
-  // t.deepEqual(path, Path.parse('/lkm'))
 })
 
 test(`Path.join - handle empty`, async t => {
@@ -36,25 +49,3 @@ test(`Path.join - handle relative path`, async t => {
   const path = Path.join('lkm')
   t.deepEqual(path.str, 'lkm')
 })
-
-test(`Path.make - handle abs path`, async t => {
-  const path = Path.make('/pok/lkm')
-  t.deepEqual(path.str, '/pok/lkm')
-  t.deepEqual(path.first, '')
-  t.deepEqual(path.rest, 'pok/lkm')
-})
-
-test(`Path.make - handle rel path`, async t => {
-  const path = Path.make('pok/lkm')
-  t.deepEqual(path.str, 'pok/lkm')
-  t.deepEqual(path.first, 'pok')
-  t.deepEqual(path.rest, 'lkm')
-})
-// // will need to be able to extend a path
-// // how does node's path lib do it? path.join?
-// test(`join .neomem`, async t => {
-//   const path = Path.make('/bookmarks')
-//   // const path2 = path.join('.neomem')
-//   // path.addTarget('.neomem')
-//   t.deepEqual(1, 1)
-// })
