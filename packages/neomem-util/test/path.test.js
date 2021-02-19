11ce10c7a6c8d51cf41b4899b2ae4a4bf3ee1beb
Brian Burns <bburns.km@gmail.com>
2021-02-19 07:20:21 -0600

    path passes

--- packages/neomem-util/test/path.test.js
+++ packages/neomem-util/test/path.test.js
@@ -1,18 +1,18 @@
 const test = require('ava').default
 const { Path } = require('../src')
 
-test(`Path.make - handle abs path`, async t => {
+test(`Path.make - should handle abs path`, async t => {
   const path = Path.make('/pok/lkm')
   t.deepEqual(path.str, '/pok/lkm')
   t.deepEqual(path.first, '')
   t.deepEqual(path.rest, 'pok/lkm')
 })
 
-test(`Path.make - handle rel path`, async t => {
-  const path = Path.make('pok/lkm')
-  t.deepEqual(path.str, 'pok/lkm')
+test(`Path.make - should handle rel path`, async t => {
+  const path = Path.make('pok/lkm/oij')
+  t.deepEqual(path.str, 'pok/lkm/oij')
   t.deepEqual(path.first, 'pok')
-  t.deepEqual(path.rest, 'lkm')
+  t.deepEqual(path.rest, 'lkm/oij')
 })
 
 test(`Path.join - should join two rel paths`, async t => {
@@ -35,17 +35,17 @@ test(`Path.join - should handle rel + abs path`, async t => {
   t.deepEqual(path.str, '/lkm')
 })
 
-test(`Path.join - handle empty`, async t => {
+test(`Path.join - should handle empty path`, async t => {
   const path = Path.join()
   t.deepEqual(path.str, '')
 })
 
-test(`Path.join - handle abs path`, async t => {
+test(`Path.join - should handle abs path`, async t => {
   const path = Path.join('/pok')
   t.deepEqual(path.str, '/pok')
 })
 
-test(`Path.join - handle relative path`, async t => {
+test(`Path.join - should handle relative path`, async t => {
   const path = Path.join('lkm')
   t.deepEqual(path.str, 'lkm')
 })
