79acafb95c6659472be028695f82f68e7c5321ea
Brian Burns <bburns.km@gmail.com>
2021-04-10 22:01:04 -0500

    works

--- packages/neomem-gateway/src/data.js
+++ packages/neomem-gateway/src/data.js
@@ -1,8 +1,8 @@
 // const environment = process.env.NODE_ENV || 'development'
 // const environment = 'development' //. for now
-
 // const { Data } from './' + environment)
 // const { Data } = await import('./' + environment)
+
 import { Data } from './development/index.js'
 
 export { Data }
