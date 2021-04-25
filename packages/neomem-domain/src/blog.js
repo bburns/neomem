27cd3aa162384b776c624d0e8d7900ade422c0f3
Brian Burns <bburns.km@gmail.com>
2021-04-25 15:37:01 -0500

    wip blog generator

--- packages/neomem-domain/src/blog.js
+++ packages/neomem-domain/src/blog.js
@@ -3,7 +3,22 @@ import data from '../data/data.js'
 import * as domain from './index.js'
 
 let { nodes, edges } = data
-// console.log(nodes)
 
 const publicNodes = R.filter(node => node.public, R.values(nodes))
 console.log(publicNodes)
+
+// const getPublic = R.filter(R.prop('public'))
+// console.log(getPublic(R.values(nodes)))
+
+function getPost(node) {
+  return `${node.name}: ${node.description || '(no description)'}`
+}
+
+const posts = R.map(getPost, publicNodes)
+
+function output(post) {
+  console.log(post)
+  console.log()
+}
+
+R.forEach(output, posts)
