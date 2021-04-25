b783063e415818a34a195572c9d6c1a0d26b397f
Brian Burns <bburns.km@gmail.com>
2021-04-25 15:50:01 -0500

    cleanup

--- packages/neomem-domain/src/blog.js
+++ packages/neomem-domain/src/blog.js
@@ -1,24 +1,16 @@
 import R from 'rambda'
 import data from '../data/data.js'
-import * as domain from './index.js'
+// import * as domain from './index.js'
 
 let { nodes, edges } = data
 
 const publicNodes = R.filter(node => node.public, R.values(nodes))
-console.log(publicNodes)
 
-// const getPublic = R.filter(R.prop('public'))
-// console.log(getPublic(R.values(nodes)))
-
-function getPost(node) {
-  return `${node.name}: ${node.description || '(no description)'}`
-}
-
-const posts = R.map(getPost, publicNodes)
-
-function output(post) {
-  console.log(post)
+function outputText(node) {
+  console.log(node.name)
+  console.log(node.created)
+  console.log(node.notes || '')
   console.log()
 }
 
-R.forEach(output, posts)
+R.forEach(outputText, publicNodes)
