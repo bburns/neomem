fcffd52e819b5366533e39bfe467850ace35232d
Brian Burns <bburns.km@gmail.com>
2021-05-08 18:05:47 -0500

    ok

--- packages/neomem-console/src/connect.js
+++ packages/neomem-console/src/connect.js
@@ -3,6 +3,7 @@ const set = () => 2
 const update = () => 3
 const del = () => 4
 
+// get a connection, which has crud fns
 export const connect = data => {
   return {
     get,
--- packages/neomem-console/src/console.test.js
+++ packages/neomem-console/src/console.test.js
@@ -7,27 +7,28 @@ const print = console.log
 const evaluate = makeConsole()
 const evaluate2 = makeConsole()
 const connection = connect(data)
+const context = { location: 'forest', connection }
 
 test(`pok`, async t => {
-  const { output } = await evaluate('pok')
+  const { output } = await evaluate('pok', context)
   t.deepEqual(output, 'huh?')
 })
 
 test(`look`, async t => {
-  const { output } = await evaluate('look')
-  t.deepEqual(output, 'i see /')
+  const { output } = await evaluate('look', context)
+  t.deepEqual(output, 'i see forest')
 })
 
 // make sure can handle two consoles at once (no singleton)
 test(`go chrome + books`, async t => {
-  const { output } = await evaluate('go chrome')
-  const { output: output2 } = await evaluate2('go books')
+  const { output } = await evaluate('go chrome', context)
+  const { output: output2 } = await evaluate2('go books', context)
   t.deepEqual(output, 'Went to chrome')
   t.deepEqual(output2, 'Went to books')
 })
 
 test(`go + look`, async t => {
-  let { context } = await evaluate('go chrome')
-  let { output } = await evaluate('look', context)
+  let { context: context2 } = await evaluate('go chrome', context)
+  let { output } = await evaluate('look', context2)
   t.deepEqual(output, 'i see chrome')
 })
