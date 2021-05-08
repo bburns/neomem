a0035ae5a90c4ebf6e2d4d14396de9a4bd221e44
Brian Burns <bburns.km@gmail.com>
2021-05-08 06:41:16 -0500

    test passes

--- packages/neomem-domain/data/data.js
+++ packages/neomem-domain/data/data.js
@@ -2,13 +2,12 @@
 
 //. call these items and relations, as the ui will use?
 
-//. use arrays? consider this the serialized form - the db would read them
-// and instantiate the indexes, or store those separately?
-// yah
+// consider this the serialized form - the db would read them
+// and instantiate the indexes, or store those separately
 
 export default {
-  nodes: {
-    1: {
+  nodes: [
+    {
       _id: 1,
       name: 'plecy',
       alias: 'plecostomus',
@@ -18,21 +17,21 @@ export default {
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    2: {
+    {
       _id: 2,
       name: 'fishes',
       type: 'folder',
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    3: {
+    {
       _id: 3,
       name: 'zoey',
       type: 'cat',
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    4: {
+    {
       _id: 4,
       name: 'default view',
       type: 'view',
@@ -40,28 +39,28 @@ export default {
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    5: {
+    {
       _id: 5,
       type: 'column',
       note: 'name',
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    6: {
+    {
       _id: 6,
       type: 'column',
       note: 'type',
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    7: {
+    {
       _id: 7,
       type: 'column',
       note: 'description',
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    8: {
+    {
       _id: 8,
       type: 'field',
       name: 'name',
@@ -69,7 +68,7 @@ export default {
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    9: {
+    {
       _id: 9,
       type: 'field',
       name: 'type',
@@ -77,7 +76,7 @@ export default {
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    10: {
+    {
       _id: 10,
       type: 'field',
       name: 'description',
@@ -85,7 +84,7 @@ export default {
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    11: {
+    {
       _id: 11,
       type: 'blog',
       name: 'neoblog',
@@ -94,7 +93,7 @@ export default {
       created: '2021-04-25T10:25:00',
       modified: null,
     },
-    12: {
+    {
       _id: 12,
       type: 'post',
       name: `Blog n++`,
@@ -103,25 +102,25 @@ export default {
       created: '2021-04-25T14:25:00',
       modified: null,
     },
-    13: {
+    {
       _id: 13,
       type: 'column',
       note: 'notes',
       created: '2021-04-25T14:25:00',
       modified: null,
     },
-  },
+  ],
 
-  edges: {
-    '1,2': {
+  edges: [
+    {
       _from: 1,
       _to: 2,
       type: 'contains',
       created: '2021-04-25T04:25:00',
       modified: null,
     },
-    '5,8': { _from: 5, _to: 8, type: 'shows' },
-    '6,9': { _from: 6, _to: 9, type: 'shows' },
-    '7,10': { _from: 7, _to: 10, type: 'shows' },
-  },
+    { _from: 5, _to: 8, type: 'shows' },
+    { _from: 6, _to: 9, type: 'shows' },
+    { _from: 7, _to: 10, type: 'shows' },
+  ],
 }
--- packages/neomem-domain/src/index.js
+++ packages/neomem-domain/src/index.js
@@ -1,11 +1,5 @@
-// crud
-
-// type Node = {
-//   _id: number,
-//   key: string,
-//   name: string,
-//   alias: string,
-// }
+// neomem domain
+// crud operations
 
 export function add(node, nodes) {
   const copy = { ...nodes }
@@ -22,7 +16,7 @@ export function get(id, nodes) {
 export function update(id, prop, value, nodes) {
   const copy = { ...nodes }
   const node = nodes[id]
-  copy[nodes._id] = { ...node, [prop]: value }
+  copy[id] = { ...node, [prop]: value }
   return copy
 }
 
@@ -30,4 +24,4 @@ export function remove(id, nodes) {
   const copy = { ...nodes }
   delete copy[id]
   return copy
- }
+}
