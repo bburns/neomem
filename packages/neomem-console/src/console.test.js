d41421cfc5a584fe89efab448714a4a51582b07d
Brian Burns <bburns.km@gmail.com>
2021-05-08 09:25:30 -0500

    wip console

--- packages/neomem-console/src/console.test.js
+++ packages/neomem-console/src/console.test.js
@@ -1,5 +1,10 @@
 import test from 'ava'
+import { makeConsole } from './index.js'
+
+const config = {}
 
 test(`pok`, async t => {
+  const console = makeConsole(config)
+  console.start()
   t.deepEqual(1, 1)
 })
