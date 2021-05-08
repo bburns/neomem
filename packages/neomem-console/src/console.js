982be1f76933b7520875b030acad5b6c58b24f5a
Brian Burns <bburns.km@gmail.com>
2021-05-08 09:48:32 -0500

    context

--- packages/neomem-console/src/console.js
+++ packages/neomem-console/src/console.js
@@ -1,47 +1,51 @@
-import repl from 'repl' // node lib - https://nodejs.org/api/repl.html
-import R from 'rambda' // functional programming lib
-import chalk from 'chalk' // color text
+import repl from 'repl' // node lib https://nodejs.org/api/repl.html
+import R from 'rambda' // functional programming lib https://ramdajs.com/
+import chalk from 'chalk' // color text https://github.com/chalk/chalk
 
 const defaultConfig = {
-  prompt: '=> ',
   welcome: `
 Welcome to Neomem
 -----------------------------------------------------`,
+  location: '/',
+  prompt: '=> ',
+}
+
+// make and return a console object. run it with console.start()
+export function makeConsole(config) {
+  config = { ...defaultConfig, ...config }
+  // const context = {
+  //   base: config.base,
+  //   location: config.location,
+  // }
+  function start() {
+    console.log(config.welcome)
+    // printLocation(context)
+    printLocation(config)
+    const replServer = repl.start({ prompt: config.prompt, eval: evalCommand })
+    // replServer.context = context // this is how you pass context to the repl?
+    replServer.context = config // this is how you pass context to the repl?
+  }
+  return {
+    start,
+  }
 }
 
 function printLocation(context) {
   console.log(chalk.bold(`\n[${context.location}]`))
 }
 
-// make and return a console object. run it with console.start()
-export function makeConsole(config) {
-  config = { ...defaultConfig, ...config }
-  const context = {
-    base: config.base,
-    location: config.location,
-  }
-  function start() {
-    console.log(config.welcome)
-    printLocation(context)
-    const replServer = repl.start({ prompt: config.prompt, eval: evalCommand })
-    // replServer.context = context // this is how you pass context to the repl?
-  }
-  // parse command string into a fn and execute it.
-  // note: these parameters are specified by node's repl library.
-  async function evalCommand(str, context, filename, callback) {
-    // const options = { context, ui, Processor }
-    // // build an action object from the user's input string
-    // const action = Action.make(str.trim(), options)
-    // try {
-    //   await Processor.execute(action) // execute the action object
-    // } catch (error) {
-    //   return callback(error)
-    // }
-    printLocation(context)
-    console.log(str)
-    callback() // so knows to print prompt again
-  }
-  return {
-    start,
-  }
+// parse command string into a fn and execute it.
+// note: these parameters are specified by node's repl library.
+async function evalCommand(str, context, filename, callback) {
+  // const options = { context, ui, Processor }
+  // // build an action object from the user's input string
+  // const action = Action.make(str.trim(), options)
+  // try {
+  //   await Processor.execute(action) // execute the action object
+  // } catch (error) {
+  //   return callback(error)
+  // }
+  printLocation(context)
+  console.log(str)
+  callback() // so knows to print prompt again
 }
--- packages/neomem-console/src/index.js
+++ packages/neomem-console/src/index.js
@@ -2,8 +2,6 @@ import { makeConsole } from './console.js'
 
 const config = {
   base: 'http://localhost:4000/',
-  location: '/',
-  // prompt: '> ',
 }
 
 const console = makeConsole(config)
