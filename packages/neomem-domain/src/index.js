f0ffe47bc9a969cc64c7d627b11718bc06712559
Brian Burns <bburns.km@gmail.com>
2021-05-08 06:52:11 -0500

    cmt

--- packages/neomem-domain/src/index.js
+++ packages/neomem-domain/src/index.js
@@ -2,6 +2,7 @@
 // crud operations
 
 export function add(node, nodes) {
+  // copy on write
   const copy = { ...nodes }
   copy[node._id] = node
   return copy
@@ -9,11 +10,13 @@ export function add(node, nodes) {
 
 //. if id is an array, return array of nodes?
 export function get(id, nodes) {
+  // return copy of item
   const copy = { ...nodes[id] }
   return copy
 }
 
 export function update(id, prop, value, nodes) {
+  // copy on write
   const copy = { ...nodes }
   const node = nodes[id]
   copy[id] = { ...node, [prop]: value }
@@ -21,6 +24,7 @@ export function update(id, prop, value, nodes) {
 }
 
 export function remove(id, nodes) {
+  // copy on write
   const copy = { ...nodes }
   delete copy[id]
   return copy
