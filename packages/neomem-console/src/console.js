4491be5344f7f7e85b9abc852899829d40fe2acf
Brian Burns <bburns.km@gmail.com>
2021-05-09 07:51:26 -0500

    move interactive stuff to indexjs

--- packages/neomem-console/src/console.js
+++ packages/neomem-console/src/console.js
@@ -1,23 +1,10 @@
 // console ui
 
-import repl from 'repl' // node lib - lots of options https://nodejs.org/api/repl.html
 import R from 'rambda' // functional programming lib https://ramdajs.com/
-import chalk from 'chalk' // color text https://github.com/chalk/chalk
 import commands from './commands.js'
 
-// data
-
-const welcome = `
-Welcome to Neomem
------------------------------------------------------`
-const prompt = '=> '
 const location = '/'
 const defaultContext = { location }
-
-// calculations
-
-const print = console.log
-const decorateLocation = location => chalk.bold(`\n[${location}]`)
 
 export const makeConsole = () => evaluate
 
@@ -33,23 +20,3 @@ const parse = tokens => {
 }
 
 const run = (cmd, context) => cmd(context)
-
-// actions
-
-export const runConsole = evaluate => {
-  print(welcome)
-  print(decorateLocation(location))
-
-  // parse command string into a fn and execute it.
-  // note: these parameters are specified by node's repl library.
-  const runStep = async (str, oldContext, filename, callback) => {
-    const { output, context } = await evaluate(str, oldContext)
-    print(output)
-    oldContext.location = context.location
-    print(decorateLocation(context.location))
-    callback() // so knows to print prompt again
-  }
-
-  const server = repl.start({ prompt, eval: runStep })
-  server.context.location = location // pass context to eval fn
-}
--- packages/neomem-console/src/index.js
+++ packages/neomem-console/src/index.js
@@ -1,3 +1,37 @@
-import { runConsole, makeConsole } from './console.js'
+import repl from 'repl' // node lib - lots of options https://nodejs.org/api/repl.html
+import chalk from 'chalk' // color text https://github.com/chalk/chalk
+import { makeConsole } from './console.js'
+import { connect } from './connect.js' // driver
+import { data } from './data.js'
 
-runConsole(makeConsole())
+const welcome = `
+Welcome to Neomem
+-----------------------------------------------------`
+const prompt = '=> '
+const location = '/'
+
+const connection = connect(data)
+
+const print = console.log
+const decorateLocation = location => chalk.bold(`\n[${location}]`)
+
+const evaluate = makeConsole()
+
+print(welcome)
+print(decorateLocation(location))
+
+// parse command string into a fn and execute it.
+// note: these parameters are specified by node's repl library.
+const runStep = async (str, oldContext, filename, callback) => {
+  const { output, context } = await evaluate(str, oldContext)
+  print(output)
+  oldContext.location = context.location
+  print(decorateLocation(context.location))
+  callback() // so knows to print prompt again
+}
+
+const server = repl.start({ prompt, eval: runStep })
+
+// pass context to eval fn
+server.context.location = location
+server.context.connection = connection
