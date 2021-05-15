11578f42cd6d0f04ced7398a29f5a1441a6c12f6
Brian Burns <bburns.km@gmail.com>
2021-05-15 14:02:56 -0500

    gitig

--- packages/neomem-domain-blog/src/blog.js
+++ packages/neomem-domain-blog/src/blog.js
@@ -7,46 +7,25 @@ const { nodes } = data
 
 //. later do some kind of query with pagination
 
-function outputPost(node) {
-  output(`<div class="post">`)
-  output(`<div class="name">${node.name}</div>`)
-  output(`<div class="created">${node.created}</div>`)
-  output(`<div class="notes">${node.notes || ''}</div>`)
-  output(`</div>`)
-}
-
 function getPost(post) {
   return `<div class='post'>
 <div class='name'>${post.name}</div>
 <div class='created'>${post.created}</div>
 <div class='notes'>${post.notes || ''}</div>
 </div>`
-  // output(`<div class="post">`)
-  // output(`<div class="name">${post.name}</div>`)
-  // output(`<div class="created">${post.created}</div>`)
-  // output(`<div class="notes">${post.notes || ''}</div>`)
-  // output(`</div>`)
-}
-
-function outputBlog(nodes) {
-  output(`<div class="blog">`)
-  R.forEach(outputPost, posts)
-  output(`</div>`)
-}
-
-function outputPage(nodes) {
-  output(`<div class="page">`)
-  outputBlog(nodes)
-  output(`</div>`)
 }
 
 function getPage(nodes) {
   return `
+<html>
+<body>
 <div class='page'>
 <div class='blog'>
 ${R.map(getPost, posts).join('\n')}
 </div>
 </div>
+</body>
+</html>
 `
 }
 
