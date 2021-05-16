3c899c7eccfd9ad90131d724161b2c7c385ee5c5
Brian Burns <bburns.km@gmail.com>
2021-05-16 01:22:29 -0500

    wip add hapi

--- packages/neomem-gateway/src/gateway.js
+++ packages/neomem-gateway/src/gateway.js
@@ -1,3 +1,34 @@
+// run a webserver to respond to get/post etc requests
+
+import Hapi from '@hapi/hapi' // web server https://hapi.dev/
+// import { Query } from 'neomem-util'
+// import { Data } from './index.js'
 import { data } from './data.js'
 
-console.log(data)
+const port = process.env.PORT || 4000
+// const apiversion1 = '/api/v1/'
+
+process.on('unhandledRejection', err => {
+  console.log(err)
+  process.exit(1)
+})
+
+const init = async () => {
+  const server = Hapi.server({ host: 'localhost', port })
+
+  server.route({
+    path: '{path*}',
+    method: 'GET',
+    handler: async (request, h) => {
+      // const query = Query.makeFromRequest(request)
+      // const json = await Data.get(query)
+      const json = data
+      return json
+    },
+  })
+
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+}
+
+init()
