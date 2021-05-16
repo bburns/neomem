9e8a07fc6a3e376f92def248193bd17e05a5c3a9
Brian Burns <bburns.km@gmail.com>
2021-05-16 01:41:49 -0500

    blog fetch data from gateway

--- packages/neomem-domain-blog/src/blog.js
+++ packages/neomem-domain-blog/src/blog.js
@@ -4,17 +4,22 @@
 // output in docs/index.md
 
 // @ts-nocheck
+import fetch from 'node-fetch'
 import R from 'rambda'
 
 const print = console.log
 
-const path = process.env.DATA_PATH || '../../neomem-gateway/src/data.js'
-const { data } = await import('./' + path)
-const { nodes } = data
+// const path = process.env.DATA_PATH || '../../neomem-gateway/src/data.js'
+// const { data } = await import('./' + path)
+// const { nodes } = data
 
 //. do some kind of query with pagination
 // const nodes = data.get({ where: { type: 'post', public: true } })
 // const nodes = data.get()
+
+const response = await fetch('http://localhost:4000')
+const data = await response.json()
+const { nodes } = data
 
 const getPost = post => `
 ## ${post.name}
