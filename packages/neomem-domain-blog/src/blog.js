baf2ebd067618136d132518707fe051d0cccf1fe
Brian Burns <bburns.km@gmail.com>
2021-05-22 09:47:20 -0500

    cleanup

--- packages/neomem-domain-blog/src/blog.js
+++ packages/neomem-domain-blog/src/blog.js
@@ -2,28 +2,21 @@
 // usage: (at top neomem folder)
 //   npm run gateway
 // (in another terminal)
-//   npm run blog && cat docs/index.md
+//   npm run blog
 
-// @ts-nocheck
+// @ts-nocheck - doesn't recognize top level await yet
 
 import fs from 'fs'
-// import libpath from 'path'
 import fetch from 'node-fetch'
-// import R from 'rambda'
 
 const print = console.log
 
 const outputFolder = process.argv[process.argv.length - 1]
 print(outputFolder)
 
-// const path = process.env.DATA_PATH || '../../neomem-gateway/src/data.js'
-// const { data } = await import('./' + path)
-// const { nodes } = data
-
 //. do some kind of query with pagination
 // const nodes = data.get({ where: { type: 'post', public: true } })
 // const nodes = data.get()
-
 const response = await fetch('http://localhost:4000')
 const data = await response.json()
 const { nodes } = data
@@ -36,25 +29,6 @@ ${post.created}
 ${post.notes || ''}
 `
 
-// these are all equivalent - what's best?
-// const getPosts = posts => R.map(getPost, posts).join('\n')
-// const getPosts = posts => R.pipe(R.map(getPost), R.join('\n'))(posts)
-// const getPosts = R.pipe(R.map(getPost), R.join('\n'))
-const getPosts = posts => posts.map(getPost).join('\n')
-
-// const posts = R.sort(
-//   (a, b) => -a.created.localeCompare(b.created),
-//   R.filter(node => node.type === 'post' && node.public, nodes)
-// )
-
-const posts = nodes
-  .filter(node => node.type === 'post' && node.public)
-  .sort((a, b) => -a.created.localeCompare(b.created))
-
-// print(getPosts(posts))
-
-// const getIndex = posts => posts.map(post => post.title)
-
 const getPostDate = post => post.created.slice(0, 10)
 
 // get rid of spaces and parens (mess up markdown rendering)
@@ -66,18 +40,24 @@ const getFileTitle = post =>
     .replace(/ /g, '-')
     .replace(/[\(\)]/g, '')
 
+const posts = nodes
+  .filter(node => node.type === 'post' && node.public)
+  .sort((a, b) => -a.created.localeCompare(b.created))
+
 let toc = `
 ## Welcome to the Neomem blog...
-
+  
 `
-// posts.forEach(post => {
+
+// write post files
 for (const post of posts) {
   const str = getPost(post)
   const fileTitle = getFileTitle(post)
-  const path = '../../' + outputFolder + '/' + fileTitle + '.md'
+  const path = `../../${outputFolder}/${fileTitle}.md`
   fs.writeFileSync(path, str)
-  toc += `- [${post.created.slice(0, 10)} ${post.name}](${fileTitle}.md)\n`
+  toc += `- [${getPostDate(post)} ${post.name}](${fileTitle}.md)\n`
 }
 
-const path = '../../' + outputFolder + '/index.md'
+// write index file
+const path = `../../${outputFolder}/index.md`
 fs.writeFileSync(path, toc)
