88722152217b88fe3980b45fbf45d659258138ab
Brian Burns <bburns.km@gmail.com>
2021-05-30 06:50:56 -0500

    blog html

--- packages/neomem-domain-blog/src/blog.js
+++ packages/neomem-domain-blog/src/blog.js
@@ -1,73 +1,34 @@
-// simple blog generator
-//
-// usage: (at top neomem folder)
-//   npm run gateway
-// (in another terminal)
-//   npm run blog
+// @ts-nocheck
+import R from 'rambda'
+import { data } from '../../neomem-console/src/data.js'
 
-// @ts-nocheck - doesn't recognize top level await yet
+const output = console.log
 
-import fs from 'fs'
-import fetch from 'node-fetch'
-
-const outputFolder = process.argv[process.argv.length - 1]
-
-//. do some kind of query with pagination
-// const nodes = data.get({ where: { type: 'post', public: true } })
-
-const response = await fetch('http://localhost:4000')
-const data = await response.json()
 const { nodes } = data
 
-// const getPost = post => `
-// ## ${post.name}
-
-// ${post.created}
-
-// ${post.notes || ''}
-// `
-const getPost = post => `---
-title: ${post.name}
-date: ${post.created}
-featured_image: images/landscape.jpg
----
-
-${post.notes || ''}
-`
-
-const getFileDate = post => post.created.slice(0, 10) // get eg '2021-05-22'
-
-// get eg 'a-blog-post'
-const getFileTitle = post =>
-  post.name
-    .toLowerCase()
-    .replace(/ /g, '-')
-    .replace(/[\(\)]/g, '') // ditch parens (cancels md to html somehow)
-    .replace(/\//g, '-')
-    .replace(/[,.]/g, '')
-
-// const getFileName = post => `${getFileDate(post)}-${getFileTitle(post)}.md`
-const getFileName = post => getFileTitle(post) + '.md'
-
-// get public posts and sort
-const posts = nodes
-  .filter(node => node.type === 'post' && node.public)
-  .sort((a, b) => -a.created.localeCompare(b.created))
-
-// let toc = `
-// ## Welcome to the Neomem blog...
-//
-//`
-
-// write post files
-for (const post of posts) {
-  const str = getPost(post)
-  const fileName = getFileName(post)
-  const path = `../../${outputFolder}/${fileName}`
-  fs.writeFileSync(path, str)
-  // toc += `- [${getFileDate(post)} ${post.name}](${fileName})\n`
+function getPost(post) {
+  return `<div class='post'>
+<div class='name'>${post.name}</div>
+<div class='created'>${post.created}</div>
+<div class='notes'>${post.notes || ''}</div>
+</div>`
 }
 
-// // write index file
-// const path = `../../${outputFolder}/index.md`
-// fs.writeFileSync(path, toc)
+function getPage(nodes) {
+  return `
+<html>
+<body>
+<div class='page'>
+<div class='blog'>
+${R.map(getPost, posts).join('\n')}
+</div>
+</div>
+</body>
+</html>
+`
+}
+
+//. later do some kind of query with pagination
+const posts = R.filter(node => node.type === 'post' && node.public, nodes)
+
+output(getPage(posts))
