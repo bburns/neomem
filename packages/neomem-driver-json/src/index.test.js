adf7bf4ee4a18a73c6062030c7cca560ac04219b
Brian Burns <bburns.km@gmail.com>
2021-05-31 00:34:39 -0500

    wip json driver

--- packages/neomem-driver-json/src/index.test.js
+++ packages/neomem-driver-json/src/index.test.js
@@ -1,39 +1,43 @@
 // run with `yarn test`
 
 import test from 'ava'
-import R from 'rambda'
-import * as domain from './index.js'
-import data from '../data/data.js'
+import * as logic from './index.js'
+import { data } from './data.js'
+
+const plecy = Object.freeze({ id: 1, props: { name: 'plecy' } })
 
 // crud operations with a const item
-test(`domain CRUD`, async t => {
+
+test(`create`, async t => {
   let nodes = {}
-  let node
-  const plecy = Object.freeze({ _id: 1, name: 'plecy' })
-
-  // create
-  nodes = domain.add(plecy, nodes)
+  nodes = logic.add(plecy, nodes)
   t.deepEqual(nodes, { 1: plecy })
+})
 
-  // retrieve
-  node = domain.get(1, nodes)
+test(`retrieve`, async t => {
+  let nodes = {}
+  let node = logic.get(1, nodes)
   t.deepEqual(node, plecy)
+})
 
-  // update
+test(`update`, async t => {
+  let nodes = {}
   const prop = 'description'
   const value = 'plecostomus'
-  nodes = domain.update(1, prop, value, nodes)
-  const plecy2 = { ...plecy, [prop]: value }
+  nodes = logic.update(1, prop, value, nodes)
+  const plecy2 = { ...plecy, props: { [prop]: value } }
   t.deepEqual(nodes, { 1: plecy2 })
+})
 
-  // delete
-  nodes = domain.remove(1, nodes)
+test(`delete`, async t => {
+  let nodes = {}
+  nodes = logic.remove(1, nodes)
   t.deepEqual(nodes, {})
 })
 
 test(`read from data.js`, async t => {
   const nodes = {}
-  R.forEach(item => (nodes[item._id] = item), data.nodes)
-  const node = domain.get(1, nodes)
-  t.like(node, { _id: 1, name: 'plecy' })
+  data.nodes.forEach(item => (nodes[item.id] = item))
+  const node = logic.get(1, nodes)
+  t.like(node, { id: 1, props: { name: 'plecy' } })
 })
