1cb019e7bc6f5e97bcc3255a471d4ddbcf4c4624
Brian Burns <bburns.km@gmail.com>
2021-05-31 01:05:04 -0500

    driver-json wip

--- packages/neomem-driver-json/src/index.js
+++ packages/neomem-driver-json/src/index.js
@@ -1,26 +1,29 @@
 // crud operations
+// these are pure functions - take an array of nodes and return an updated copy.
 
-export function add(node, nodes) {
-  const copy = { ...nodes }
-  copy[node._id] = node
-  return copy
-}
+import R from 'rambda'
 
-//. if id is an array, return array of nodes?
+// export function add(node, nodes) {
+//   const copy = R.clone(nodes)
+//   copy[node.id] = node
+//   return copy
+// }
+
 export function get(id, nodes) {
-  const copy = { ...nodes[id] }
-  return copy
+  const node = nodes.find(node => node.id === id)
+  return node
 }
 
-export function update(id, prop, value, nodes) {
-  const copy = { ...nodes }
-  const node = nodes[id]
-  copy[id] = { ...node, [prop]: value }
-  return copy
-}
+// export function update(id, prop, value, nodes) {
+//   const copy = R.clone(nodes)
+//   const node = nodes[id]
+//   const props = 0
+//   copy[id] = { ...node, [prop]: value }
+//   return copy
+// }
 
-export function remove(id, nodes) {
-  const copy = { ...nodes }
-  delete copy[id]
-  return copy
-}
+// export function remove(id, nodes) {
+//   const copy = R.clone(nodes)
+//   delete copy[id]
+//   return copy
+// }
--- packages/neomem-driver-json/src/index.test.js
+++ packages/neomem-driver-json/src/index.test.js
@@ -1,43 +1,43 @@
 // run with `yarn test`
 
 import test from 'ava'
-import * as logic from './index.js'
+import * as driver from './index.js'
 import { data } from './data.js'
 
 const plecy = Object.freeze({ id: 1, props: { name: 'plecy' } })
 
 // crud operations with a const item
 
-test(`create`, async t => {
-  let nodes = {}
-  nodes = logic.add(plecy, nodes)
-  t.deepEqual(nodes, { 1: plecy })
-})
+// test(`create`, async t => {
+//   let nodes = {}
+//   nodes = driver.add(plecy, nodes)
+//   t.deepEqual(nodes, { 1: plecy })
+// })
 
 test(`retrieve`, async t => {
-  let nodes = {}
-  let node = logic.get(1, nodes)
+  let nodes = [plecy]
+  let node = driver.get(1, nodes)
   t.deepEqual(node, plecy)
 })
 
-test(`update`, async t => {
-  let nodes = {}
-  const prop = 'description'
-  const value = 'plecostomus'
-  nodes = logic.update(1, prop, value, nodes)
-  const plecy2 = { ...plecy, props: { [prop]: value } }
-  t.deepEqual(nodes, { 1: plecy2 })
-})
+// test(`update`, async t => {
+//   let nodes = {}
+//   const prop = 'description'
+//   const value = 'plecostomus'
+//   nodes = driver.update(1, prop, value, nodes)
+//   const plecy2 = { ...plecy, props: { [prop]: value } }
+//   t.deepEqual(nodes, { 1: plecy2 })
+// })
 
-test(`delete`, async t => {
-  let nodes = {}
-  nodes = logic.remove(1, nodes)
-  t.deepEqual(nodes, {})
-})
+// test(`delete`, async t => {
+//   let nodes = {}
+//   nodes = driver.remove(1, nodes)
+//   t.deepEqual(nodes, {})
+// })
 
-test(`read from data.js`, async t => {
-  const nodes = {}
-  data.nodes.forEach(item => (nodes[item.id] = item))
-  const node = logic.get(1, nodes)
-  t.like(node, { id: 1, props: { name: 'plecy' } })
-})
+// test(`read from data.js`, async t => {
+//   const nodes = {}
+//   data.nodes.forEach(item => (nodes[item.id] = item))
+//   const node = driver.get(1, nodes)
+//   t.like(node, { id: 1, props: { name: 'plecy' } })
+// })
