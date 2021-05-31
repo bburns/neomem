84ea80333a7c73260701778cadd67cec9eb84913
Brian Burns <bburns.km@gmail.com>
2021-05-31 08:01:38 -0500

    about

--- packages/neomem-domain-site/src/site.js
+++ packages/neomem-domain-site/src/site.js
@@ -4,6 +4,20 @@ const print = console.log
 
 const { nodes } = data
 
+const about = `
+Neomem is a combination table and document editor, with free and paid hosting plans and a marketplace for plugins.
+
+You can filter, sort, group, and edit data from different sources in the same way - files, bookmarks, email, database, etc.
+
+It's open source and cross-platform. More information at https://github.com/bburns/Neomem.
+
+Development is ongoing, with a launch planned for August 2021.
+
+Follow along on Twitter - https://twitter.com/bburnskm and https://twitter.com/neomem_io.
+`
+  .trim()
+  .replaceAll('\n', '<br/>')
+
 function getPost(post) {
   return `<div class='post'>
 <div class='name'><b>${post.props.name}</b></div>
@@ -16,9 +30,12 @@ function getPage(nodes) {
   return `<!doctype html>
 <html>
 <head>
+<title>Neomem.io</title>
 <style>
 .title {
   font-size: 2em;
+}
+.about {
   margin-bottom: 1em;
 }
 .post {
@@ -28,7 +45,10 @@ function getPage(nodes) {
 </head>
 <body>
 <div class='page'>
-<div class='title'>Neomem Blog</div>
+<div class='title'>Neomem</div>
+<div class='about'>
+${about}
+</div>
 <div class='blog'>
 ${posts.map(getPost).join('\n')}
 </div>
