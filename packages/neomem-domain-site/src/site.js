632b77174590ec026d3ef751646bc15a1c2d40af
Brian Burns <bburns.km@gmail.com>
2021-05-31 07:45:39 -0500

    ctd

--- packages/neomem-domain-site/src/site.js
+++ packages/neomem-domain-site/src/site.js
@@ -7,7 +7,7 @@ const { nodes } = data
 function getPost(post) {
   return `<div class='post'>
 <div class='name'><b>${post.props.name}</b></div>
-<div class='created'>${post.props.created}</div>
+<div class='created'>${post.props.created.slice(0, 10)}</div>
 <div class='notes'>${post.props.notes.replaceAll('\n', '<br/>')}</div>
 </div>`
 }
@@ -17,6 +17,10 @@ function getPage(nodes) {
 <html>
 <head>
 <style>
+.title {
+  font-size: 2em;
+  margin-bottom: 1em;
+}
 .post {
   margin-bottom: 2em;
 }
@@ -24,6 +28,9 @@ function getPage(nodes) {
 </head>
 <body>
 <div class='page'>
+<div class='title'>
+Neomem Blog
+</div>
 <div class='blog'>
 ${posts.map(getPost).join('\n')}
 </div>
