1061d0e765a5ab912ebb8baff0deabafbc0e9d1f
Brian Burns <bburns.km@gmail.com>
2021-05-31 01:23:37 -0500

    wip switch to more oo approach

--- packages/neomem-driver-json/src/index.js
+++ packages/neomem-driver-json/src/index.js
@@ -1,26 +1,22 @@
 // crud operations
 // these are pure functions - take an array of nodes and return an updated copy.
 
-import R from 'rambda'
+let nodes = []
 
-// export function add(node, nodes) {
-//   const copy = R.clone(nodes)
-//   copy[node.id] = node
-//   return copy
-// }
+export function add(node) {
+  nodes.push(node)
+}
 
-export function get(id, nodes) {
+export function get(id) {
   const node = nodes.find(node => node.id === id)
   return node
 }
 
-// export function update(id, prop, value, nodes) {
-//   const copy = R.clone(nodes)
-//   const node = nodes[id]
-//   const props = 0
-//   copy[id] = { ...node, [prop]: value }
-//   return copy
-// }
+export function update(id, prop, value) {
+  const node = get(id) || {}
+  const props = node.props
+  props[prop] = value
+}
 
 // export function remove(id, nodes) {
 //   const copy = R.clone(nodes)
--- packages/neomem-driver-json/src/index.test.js
+++ packages/neomem-driver-json/src/index.test.js
@@ -2,32 +2,29 @@
 
 import test from 'ava'
 import * as driver from './index.js'
-import { data } from './data.js'
 
 const plecy = Object.freeze({ id: 1, props: { name: 'plecy' } })
 
-// crud operations with a const item
+// crud operations
 
-// test(`create`, async t => {
-//   let nodes = {}
-//   nodes = driver.add(plecy, nodes)
-//   t.deepEqual(nodes, { 1: plecy })
-// })
-
-test(`retrieve`, async t => {
-  let nodes = [plecy]
-  let node = driver.get(1, nodes)
-  t.deepEqual(node, plecy)
+test(`create`, async t => {
+  driver.add(plecy)
+  t.deepEqual(driver.get(1), plecy)
 })
 
-// test(`update`, async t => {
-//   let nodes = {}
-//   const prop = 'description'
-//   const value = 'plecostomus'
-//   nodes = driver.update(1, prop, value, nodes)
-//   const plecy2 = { ...plecy, props: { [prop]: value } }
-//   t.deepEqual(nodes, { 1: plecy2 })
-// })
+test(`retrieve`, async t => {
+  driver.add(plecy)
+  t.deepEqual(driver.get(1), plecy)
+})
+
+test(`update`, async t => {
+  const prop = 'notes'
+  const value = 'plecostomus'
+  driver.update(1, prop, value)
+  //. yuck
+  const plecy2 = { ...plecy, props: { ...plecy.props, [prop]: value } }
+  t.deepEqual(driver.get(1), plecy2)
+})
 
 // test(`delete`, async t => {
 //   let nodes = {}
