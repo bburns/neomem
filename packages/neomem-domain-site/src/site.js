51002a86869ecaefcbd957a608f0e77b99f6e71a
Brian Burns <bburns.km@gmail.com>
2021-06-01 21:04:35 -0500

    wip

--- packages/neomem-domain-site/src/site.js
+++ packages/neomem-domain-site/src/site.js
@@ -1,6 +1,7 @@
 // site/blog generator
 // npm run build
 
+import fs from 'fs'
 import { data } from '../../neomem-driver-json/src/data.js'
 import marked from 'marked'
 
@@ -8,22 +9,10 @@ const print = console.log
 
 const { nodes } = data
 
-//. get from blog node - get({ name: 'neomem blog' }) ?
-const about = `
-Neomem is a combination table and document editor, with free and paid hosting plans and a marketplace for plugins.
-
-You can filter, sort, group, and edit data from different sources in the same way - files, bookmarks, email, database, etc.
-
-It's open source and cross-platform. More information at https://github.com/bburns/Neomem.
-
-Development is ongoing, with a launch planned for August 2021.
-
-Follow along on Twitter - https://twitter.com/bburnskm and https://twitter.com/neomem_io.
-
-Note: this site is generated in Neomem - will make it look nicer later...
-
-----
-`
+//. use a query like get({ name: 'Neomem blog' })
+const about = marked(
+  nodes.find(node => node.name === 'Neomem blog').props.notes
+)
 
 function getPost(post) {
   return `<div class='post'>
@@ -33,23 +22,12 @@ function getPost(post) {
 </div>`
 }
 
-function getPage(nodes) {
-  return `<!doctype html>
-<html>
-<head>
-<title>Neomem.io</title>
-<style>
-.title {
-  font-size: 3em;
-}
-.about {
-  margin-bottom: 1em;
-}
-.post {
-  margin-bottom: 2em;
-}
-</style>
-</head>
+//. get nodes via edges from the main blog node
+const posts = nodes
+  .filter(node => node.props.type === 'post' && node.props.public)
+  .sort((a, b) => -a.created.localeCompare(b.created))
+
+const body = `
 <body>
 <div class='page'>
 <div class='title'>Neomem</div>
@@ -61,12 +39,10 @@ ${posts.map(getPost).join('\n')}
 </div>
 </div>
 </body>
-</html>`
-}
+`
 
-//. get nodes via edges from the main blog node
-const posts = nodes
-  .filter(node => node.props.type === 'post' && node.props.public)
-  .sort((a, b) => -a.created.localeCompare(b.created))
+const templatePath = `site/templates/index.html`
+const template = String(fs.readFileSync(templatePath))
+const page = template.replace('{{body}}', body)
 
-print(getPage(posts))
+print(page)
