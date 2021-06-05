552c70c3145f836d8728659d284f98d0695750d6
Brian Burns <bburns.km@gmail.com>
2021-06-05 07:28:50 -0500

    look

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import repl from 'repl' // node lib - lots of options https://nodejs.org/api/repl.html
 
+const print = console.log
 const welcome = `
 Welcome to Neomem
 -----------------------------------------------------`
 const prompt = '=> '
 
-console.log(welcome)
+print(welcome)
+
+let data = {
+  nodes: [{ name: 'a forest' }],
+}
 
 // parse command string into a fn and execute it.
 // note: these parameters are specified by node's repl library.
 const step = async (str, oldContext, filename, callback) => {
+  str = str.trim()
+  if (str === 'look' || str === 'l') {
+    print(data.nodes)
+  }
   // const { output, context } = await evaluate(str, oldContext)
   // print(output)
   // oldContext.locationId = context.locationId
