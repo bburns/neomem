f26b9528d43153879e840867211edf12d28235c7
Brian Burns <bburns.km@gmail.com>
2021-06-12 05:22:18 -0500

    start gsheet

--- src/drivers/index.js
+++ src/drivers/index.js
@@ -1,6 +1,7 @@
 import { driver as filesys } from './filesys/index.js'
 import { driver as jsonTimegraph } from './json-timegraph/index.js'
 import { driver as markdown } from './markdown/index.js'
+import { driver as gsheet } from './gsheet/index.js'
 // import { driver as chrome } from './chrome/index.js'
 // import {driver as postgres} from './postgres/index.js'
 
@@ -8,6 +9,7 @@ export const drivers = {
   filesys,
   jsonTimegraph,
   markdown,
+  gsheet,
   // chrome,
   // postgres,
 }
