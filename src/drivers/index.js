a0aa5c45e577929282a2ea79a618142436066559
Brian Burns <bburns.km@gmail.com>
2021-06-12 06:20:02 -0500

    orgmode list

--- src/drivers/index.js
+++ src/drivers/index.js
@@ -1,7 +1,8 @@
 import { driver as filesys } from './filesys/index.js'
 import { driver as jsonTimegraph } from './json-timegraph/index.js'
 import { driver as markdown } from './markdown/index.js'
-import { driver as gsheet } from './gsheet/index.js'
+import { driver as orgmode } from './orgmode/index.js'
+// import { driver as gsheet } from './gsheet/index.js'
 // import { driver as chrome } from './chrome/index.js'
 // import {driver as postgres} from './postgres/index.js'
 
@@ -9,7 +10,8 @@ export const drivers = {
   filesys,
   jsonTimegraph,
   markdown,
-  gsheet,
+  orgmode,
+  // gsheet,
   // chrome,
   // postgres,
 }
--- src/drivers/orgmode/index.js
+++ src/drivers/orgmode/index.js
@@ -8,6 +8,8 @@ export const driver = {
   },
 }
 
+//
+
 class Connection {
   constructor() {
     this.type = 'orgmode'
