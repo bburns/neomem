59bb069016c530e679d9536e2a81ec74f9bb2b89
Brian Burns <bburns.km@gmail.com>
2021-06-12 05:44:16 -0500

    start orgmode

---
+++ src/drivers/orgmode/index.js
@@ -0,0 +1,77 @@
+// driver for orgmode text files
+
+import fs from 'fs/promises'
+
+export const driver = {
+  connect() {
+    return new Connection()
+  },
+}
+
+class Connection {
+  constructor() {
+    this.type = 'orgmode'
+    this.path = null
+    this.initialLocation = null
+    this.text = null
+  }
+
+  async load(path) {
+    this.path = path
+    //. read whole file
+    this.text = String(await fs.readFile(path))
+  }
+
+  getInitialLocation() {
+    return this.initialLocation
+  }
+
+  async get(key) {
+    //. scan file for key = header name/text - eventually could have indexes?
+    //. returns this.text ?
+    const props = { _id: key, name: this.path, notes: this.text }
+    return new Node(this, props)
+  }
+  set() {}
+  update() {}
+  del() {}
+}
+
+//
+
+class Node {
+  constructor(connection, props = {}) {
+    this.connection = connection
+    this.props = props
+  }
+
+  getContents() {
+    const regex = /^[*]+[ ]+.*$/gm // match header lines
+    const contents = []
+    let arr
+    while ((arr = regex.exec(this.props.notes)) !== null) {
+      contents.push(arr[0])
+    }
+    return contents
+  }
+
+  // getNotes() {
+  //   return this.props.notes
+  // }
+
+  getType() {
+    const type = { _id: 'orgmode', name: 'orgmode' }
+    return new Node(this.connection, type)
+  }
+
+  // some props are simple keyvalue items, some are relnships, etc
+  async get(prop) {
+    const map = {
+      contents: this.getContents,
+      // notes: this.getNotes,
+      type: this.getType,
+    }
+    const method = map[prop]
+    return method ? method.bind(this)() : this.props[prop]
+  }
+}
