d28686b6a45aefbe3515add2c63941796ac35e87
Brian Burns <bburns.km@gmail.com>
2021-06-13 10:59:21 -0500

    wip handle go

--- src/commands/libcommands.js
+++ src/commands/libcommands.js
@@ -2,6 +2,7 @@ import { drivers } from '../drivers/index.js'
 import * as lib from '../lib.js'
 
 // get a destination for a command
+
 //. words could specify nothing, an adjacent edge name / direction,
 // node name, abs path, id, rownum, adjective+noun, 'back', 'fwd',
 // or something in the location history / context, connection string, etc.
@@ -17,12 +18,13 @@ import * as lib from '../lib.js'
 export async function getDestination({ location, words, past, table }) {
   let path = words[1] || location.path
 
-  //. get location from rownum in previous table
-  if (lib.isNumber(path)) {
-    path = table[Number(path)][1] //..
-  }
+  // //. get location from rownum in previous table
+  // if (lib.isNumber(path)) {
+  //   path = table[Number(path)][1] //..
+  // }
+
   // get node of new location
-  const node = await location.datasource.get(location.path)
+  const node = await location.datasource.get(path)
   const type = await node.get('type')
 
   // if new node is a mount point, replace it with the target
@@ -30,7 +32,7 @@ export async function getDestination({ location, words, past, table }) {
   if (type === 'mount') {
     const driverName = await node.get('driver')
     const driver = drivers[driverName]
-    location.datasource = await driver.connect(location)
+    location.datasource = await driver.connect(path)
     location.path = await location.datasource.get('initialPath')
   }
 
