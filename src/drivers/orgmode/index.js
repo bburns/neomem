e31d56def6a606bcbfc7b6e1c3ffe165b3ded69d
Brian Burns <bburns.km@gmail.com>
2021-06-15 20:20:38 -0500

    wip nowork

--- src/drivers/orgmode/index.js
+++ src/drivers/orgmode/index.js
@@ -17,16 +17,50 @@ class DatasourceOrgmode {
     this.type = 'orgmode'
     this.path = path
     this.initialPath = path
+    this.index = {} //.
+    this.text = null
+    this.dirty = true
+  }
+
+  // load file and scan for headers, add to indexes
+  async load() {
+    // load file
+    this.text = String(await fs.readFile(this.path))
+    // add node for front matter
+    const node = { key: 0, name: '', type: 'orgmode' }
+    const subnodes = [node]
+    // scan file for headers
+    const regex = /^([*]+)[ ]+(.*)$/gm // match header lines
+    const type = 'header'
+    let arr
+    while ((arr = regex.exec(this.text)) !== null) {
+      const nchar = 1 //.
+      const length = 100 //.
+      const key = nchar
+      const indent = arr[1]
+      const depth = arr[1].length
+      const header = arr[2]
+      const text = 'pokpok' //. this.text.slice(nchar, nchar + length)
+      const node = { key, name: header, depth, indent, type, length, text }
+      subnodes.push(node)
+    }
+    // update indexes
+    this.index.keys = {}
+    for (const node of subnodes) {
+      this.index.keys[node.key] = node
+    }
+    this.subnodes = subnodes
+    this.dirty = false
   }
 
   // crud operations
 
   async get(spec) {
+    if (this.dirty) await this.load()
     let key = spec
     if (key === 'initialPath') return this.initialPath
-    const name = key //'' //.
-    const type = name ? 'heading' : 'orgmode'
-    const props = { _id: key, name, path: key, type }
+    // const props = { _id: key, name, path: key, type } //.
+    const props = this.index.keys[key]
     return new NodeOrgmode(this, props)
   }
   set() {}
@@ -42,24 +76,7 @@ class NodeOrgmode {
     this.props = props
   }
 
-  async load() {
-    this.props.notes = String(await fs.readFile(this.datasource.path))
-  }
-
-  //. scan file for key = header name/text - then add indexes
-  async getContents() {
-    if (!this.props.notes) await this.load()
-    const regex = /^[*]+[ ]+(.*)$/gm // match header lines
-    const contents = []
-    let arr
-    while ((arr = regex.exec(this.props.notes)) !== null) {
-      contents.push(arr[1])
-    }
-    return contents
-  }
-
   async getNotes() {
-    if (!this.props.notes) await this.load()
     return this.props.notes
   }
 
@@ -67,7 +84,7 @@ class NodeOrgmode {
 
   async get(spec) {
     const map = {
-      contents: this.getContents,
+      // contents: this.getContents,
       notes: this.getNotes,
     }
     return libdrivers.get(this, spec, map)
