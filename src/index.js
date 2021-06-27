ec77ab5d7ecc3bc4ffab7cb6c60d8189d151fdba
Brian Burns <bburns.km@gmail.com>
2021-06-27 06:35:54 -0500

    swap

--- src/index.js
+++ src/index.js
@@ -1,62 +1,45 @@
-// main console repl
+// console ui
+// based on https://nodejs.org/api/readline.html#readline_example_tiny_cli
+// originally used https://nodejs.org/api/repl.html but couldn't get it to work with text editor
 
-import librepl from 'repl' // node lib - lots of options https://nodejs.org/api/repl.html
+import libreadline from 'readline' // node lib
+import { execSync } from 'child_process' // node lib
 import chalk from 'chalk' // color text https://github.com/chalk/chalk
 import { drivers } from './drivers/index.js' // data source drivers
 import { commands, aliases } from './commands/index.js' // command handlers
 
-const filepath = './src/data/index.js' //. pass via envar or param
-const filedriver = 'jsonTimegraph' //. ditto, until can automate it
-//. const connectionString = 'file://src/data/index.js'
+const readline = libreadline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> ',
+})
 
-//. make a ui object
-const print = console.log
+readline.prompt()
 
-const welcome = `
-Welcome to Neomem
------------------------------------------------------`
+readline.on('line', line => {
+  switch (line.trim()) {
+    case 'hello':
+      console.log('world!')
+      break
+    case 'edit':
+      // const shell = 'sh -c '
+      const shell = ''
+      const editor = 'micro' // works
+      // const editor = 'nano' // nowork - freezes terminal
+      const path = 'pokpok'
+      const cmd = `${shell}${editor} ${path}`
+      const result = execSync(cmd).toString()
+      console.log({ result })
+      break
 
-async function main() {
-  //. let datasource = drivers.connect(connectionString)
-  let datasource = drivers[filedriver].connect(filepath)
-  let path = await datasource.get('initialPath')
-  let location = { datasource, path }
-  let table = null
-
-  print(welcome)
-  const ret = await commands.look({ location })
-  print(ret.output)
-  print()
-
-  const getPrompt = location =>
-    `${chalk.bold(
-      '[' + location.datasource.type + '://' + location.path + ']'
-    )}\n> `
-  const prompt = getPrompt(location)
-  const past = [location] // array of previous locations
-
-  // parse and execute command string
-  // note: these parameters are specified by node's repl library.
-  async function step(str, context, filename, callback) {
-    str = str.trim()
-    const words = str.split(' ') //. tokenize
-    const command = words[0]
-    const commandFn = commands[command] || aliases[command] || commands.unknown
-    const ret = await commandFn({ location, words, past, table }) // execute cmd
-    // update vars if needed
-    if (ret) {
-      if (ret.location) location = ret.location
-      if (ret.table) table = ret.table
-      if (ret.output) {
-        print(ret.output)
-      }
-    }
-    print()
-    repl.setPrompt(getPrompt(location))
-    callback() // so knows to print prompt again
+    default:
+      console.log(`Say what? I might have heard '${line.trim()}'`)
+      break
   }
+  readline.prompt()
+})
 
-  const repl = librepl.start({ prompt, eval: step })
-}
-
-main()
+readline.on('close', () => {
+  console.log('Have a great day!')
+  process.exit(0)
+})
