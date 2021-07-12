79f24560414d4d9e9d257a03e79589a09116ca41
Brian Burns <bburns.km@gmail.com>
2021-07-12 02:16:31 -0500

    cmt

--- src/views/properties.js
+++ src/views/properties.js
@@ -11,109 +11,47 @@ class PropertiesView {
     this.source = source
   }
 
-  // get rows of text
-  //. not getLines?
+  // get rows of text from objs from datasource.
+  // properties go vertically, objects horizontally.
+  // can be one object, as with look command, or >1, as with compare cmd.
+  //. not getLines? how handle notes property? might be nice to be multiline -
+  // ie diff props could have diff heights.
+  //. handle spacing - i guess assume monospace
   async *getRows(start, count) {
+    //. header may have multiple objs - distinguish by name (instead of 'value')
     const header = ['property', 'value'].join('  |  ')
     yield header
     const line = `-----------------------------------------------`
     yield line
-    const objs = await this.source.getObjs(start, count) // get projections for each node
+    // get generator of projections for each node
+    const objs = await this.source.getObjs(start, count)
+    //. make objs a list - find better way
     const objs2 = []
     for await (let obj of objs) {
       objs2.push(obj)
     }
-    const { columns } = this.source.meta
+    const { columns } = this.source.meta //. -> properties?
+    // iterate down properties (vertically)
     for (const column of columns) {
       const values = []
+      // iterate across objects (horizontally)
       for await (let obj of objs2) {
+        //. this is all in progress -
+        // the value of an object property can be diff types - how handle them all?
         const oc = obj[column]
         if (Array.isArray(oc)) {
           //. return an array? a string?
           const value = oc.map(o => (typeof o === 'object' ? o.props.name : o))
           values.push(value)
         } else if (typeof oc === 'object') {
-          values.push(oc.props ? oc.props.name : '??')
+          values.push(oc.props ? oc.props.name : '??') //.
         } else {
           values.push(oc)
         }
       }
+      // yield the row of text
       const str = [column, ...values].join('  |  ')
       yield str
     }
-
-    // for await (let obj of objs) {
-    //   for (let column of columns) {
-    //     const oc = obj[column]
-    //     if (Array.isArray(oc)) {
-    //       //. return an array? a string?
-    //       const value = oc.map(o => (typeof o === 'object' ? o.props.name : o))
-    //       values.push(value)
-    //     } else if (typeof oc === 'object') {
-    //       values.push(oc.props.name)
-    //     } else {
-    //       values.push(oc)
-    //     }
-    //   }
-    // }
   }
 }
-
-// // convert a list of node objects to a property table
-// //. call objs nodes? or are these prop objs?
-// export async function properties({ objs, meta }) {
-//   const { columns } = meta
-//   // const rows = [['property', 'value']]
-//   // const rows = [{ property: 'property', value: ['value'] }]
-//   const rows = { property: ['value'] }
-//   for (const column of columns) {
-//     // const values = objs.map(obj => obj[column])
-//     const values = objs.map(obj => {
-//       const oc = obj[column]
-//       // typeof obj[column] === 'object' ? obj[column].name : obj[column]
-//       // const type = typeof obj[column]
-//       if (Array.isArray(oc)) {
-//         // return an array? a string?
-//         // return oc.map(o => o.name).join(', ')
-//         // return oc.join(', ')
-//         const value = oc.map(o => {
-//           if (typeof o === 'object') {
-//             return o.props.name
-//           }
-//           return o
-//         })
-//         return value
-//       } else if (typeof oc === 'object') {
-//         return oc.props.name
-//       }
-//       return oc
-//     })
-//     // const row = [column, ...values]
-//     // const row = { property: column, value: values }
-//     // rows.push(row)
-//     rows[column] = values
-//   }
-//   return rows
-// }
-
-// export async function properties({ node, meta, axis = null }) {
-//   const subnodes = [node]
-//   if (axis) {
-//     const keys = await node.get(axis) // eg get('contents') -> array of itemkeys
-//     // const path = (await node.get('path')) || '.'
-//     for (const key of keys) {
-//       // const subpath = path + '/' + key //. use this
-//       const subpath = key
-//       const subnode = await node.datasource.get(subpath)
-//       subnodes.push(subnode)
-//     }
-//   }
-//   const { columns } = meta
-//   const objs = []
-//   for (const [n, subnode] of subnodes.entries()) {
-//     const obj = await subnode.get(columns)
-//     obj.n = n
-//     objs.push(obj)
-//   }
-//   return objs
-// }
